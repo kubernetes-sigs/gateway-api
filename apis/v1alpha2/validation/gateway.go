907a029f50311025c45bfb9f983402b858b59cbd
Saurabh Bisht <bishtsaurabh5@gmail.com>
2021-08-31 17:23:10 +0530

    Add protocol specific validation for Gateway api
    This commit adds the following validations
    
    - TLSConfig is empty when the protocol is HTTP, TCP, or UDP.
    - Hostname is empty when the protocol is TCP or UDP.

--- apis/v1alpha2/validation/gateway.go
+++ apis/v1alpha2/validation/gateway.go
@@ -17,9 +17,25 @@ limitations under the License.
 package validation
 
 import (
+	"fmt"
+
 	"k8s.io/apimachinery/pkg/util/validation/field"
 
 	gatewayv1a2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
+)
+
+var (
+	// set of protocols for which we need to validate that hostname is empty
+	protocolsHostnameInvalid = map[gatewayv1a2.ProtocolType]struct{}{
+		gatewayv1a2.TCPProtocolType: {},
+		gatewayv1a2.UDPProtocolType: {},
+	}
+	// set of protocols for which TLSConfig shall not be present
+	protocolsTLSInvalid = map[gatewayv1a2.ProtocolType]struct{}{
+		gatewayv1a2.HTTPProtocolType: {},
+		gatewayv1a2.UDPProtocolType:  {},
+		gatewayv1a2.TCPProtocolType:  {},
+	}
 )
 
 // ValidateGateway validates gw according to the Gateway API specification.
@@ -29,5 +45,47 @@ import (
 // Validation that is not possible with CRD annotations may be added here in the future.
 // See https://github.com/kubernetes-sigs/gateway-api/issues/868 for more information.
 func ValidateGateway(gw *gatewayv1a2.Gateway) field.ErrorList {
-	return nil
+	return validateGatewaySpec(&gw.Spec, field.NewPath("spec"))
+}
+
+// validateGatewaySpec validates whether required fields of spec are set according to the
+// Gateway API specification.
+func validateGatewaySpec(spec *gatewayv1a2.GatewaySpec, path *field.Path) field.ErrorList {
+	return validateGatewayListeners(spec.Listeners, path.Child("listeners"))
+}
+
+// validateGatewayListeners validates whether required fields of listeners are set according
+// to the Gateway API specification.
+func validateGatewayListeners(listeners []gatewayv1a2.Listener, path *field.Path) field.ErrorList {
+	var errs field.ErrorList
+	errs = append(errs, validateListenerTLSConfig(listeners, path)...)
+	errs = append(errs, validateListenerHostname(listeners, path)...)
+	return errs
+}
+
+func validateListenerTLSConfig(listeners []gatewayv1a2.Listener, path *field.Path) field.ErrorList {
+	var errs field.ErrorList
+	for i, l := range listeners {
+		if isProtocolInSubset(l.Protocol, protocolsTLSInvalid) && l.TLS != nil {
+			errs = append(errs, field.Forbidden(path.Index(i).Child("tls"), fmt.Sprintf("should be empty for protocol %v", l.Protocol)))
+		}
+	}
+	return errs
+}
+
+func isProtocolInSubset(protocol gatewayv1a2.ProtocolType, set map[gatewayv1a2.ProtocolType]struct{}) bool {
+	_, ok := set[protocol]
+	return ok
+}
+
+// validateListenerHostname validates each listener hostname
+// should be empty in case protocol is TCP or UDP
+func validateListenerHostname(listeners []gatewayv1a2.Listener, path *field.Path) field.ErrorList {
+	var errs field.ErrorList
+	for i, h := range listeners {
+		if isProtocolInSubset(h.Protocol, protocolsHostnameInvalid) && h.Hostname != nil {
+			errs = append(errs, field.Forbidden(path.Index(i).Child("hostname"), fmt.Sprintf("should be empty for protocol %v", h.Protocol)))
+		}
+	}
+	return errs
 }
