dea7237d7e238ff560d98f5ef4bad2737521fdc6
Nathan Coleman <nathan.coleman@hashicorp.com>
2022-04-14 18:14:08 -0400

    Stub 3 gateway->secret tests cases

---
+++ conformance/tests/gateway-secret-missing-reference-policy.go
@@ -0,0 +1,37 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tests
+
+import (
+	"testing"
+
+	"sigs.k8s.io/gateway-api/conformance/utils/suite"
+)
+
+func init() {
+	ConformanceTests = append(ConformanceTests, GatewaySecretMissingReferencePolicy)
+}
+
+var GatewaySecretMissingReferencePolicy = suite.ConformanceTest{
+	ShortName:   "GatewaySecretMissingReferencePolicy",
+	Description: "A Gateway in the gateway-conformance-infra namespace should fail to become ready if the Gateway has a certificateRef for a Secret in the gateway-conformance-web-backend namespace and a ReferencePolicy granting permission to the Secret does not exist",
+	Features:    []suite.SupportedFeature{suite.SupportReferencePolicy},
+	Manifests:   []string{"tests/gateway-secret-missing-reference-policy.yaml"},
+	Test: func(t *testing.T, s *suite.ConformanceTestSuite) {
+
+	},
+}
