62e0cb431c86e84fba0c87dd200c0c4119f2fdd1
Nathan Coleman <nathan.coleman@hashicorp.com>
2022-06-06 19:04:28 -0400

    Fix goimports blocking issues

--- conformance/tests/gateway-secret-reference-policy-all-in-namespace.go
+++ conformance/tests/gateway-secret-reference-policy-all-in-namespace.go
@@ -21,6 +21,7 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+
 	"sigs.k8s.io/gateway-api/apis/v1alpha2"
 	"sigs.k8s.io/gateway-api/conformance/utils/kubernetes"
 	"sigs.k8s.io/gateway-api/conformance/utils/suite"
