1bc31f2646ffbded5704f1e9c7178b752bdca4db
toby cabot <toby@acnodal.io>
2022-06-08 09:57:38 -0400

    Change ReferenceGrant shortname from "refpol" to "refgrant"
    I couldn't think of an abbreviation for "grant" so I went with the
    whole word.

--- apis/v1alpha2/referencegrant_types.go
+++ apis/v1alpha2/referencegrant_types.go
@@ -20,7 +20,7 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 // +genclient
 // +kubebuilder:object:root=true
-// +kubebuilder:resource:categories=gateway-api,shortName=refpol
+// +kubebuilder:resource:categories=gateway-api,shortName=refgrant
 // +kubebuilder:storageversion
 // +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`
 
