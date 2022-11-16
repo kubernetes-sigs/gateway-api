7a1324159a500c08596f5458e936f3bb65ef8592
Wilson Wu <iwilsonwu@gmail.com>
2022-11-16 16:02:12 +0800

    fix docker buildx

--- hack/build-and-push.sh
+++ hack/build-and-push.sh
@@ -67,10 +67,11 @@ fi
 # And, we add an extra version tag - either :latest or semver.
 # The buildx integrate build and push in one line.
 docker buildx build \
-    --platform linux/amd64,linux/arm64 \
+    -f Dockerfile \
+    -t ${REGISTRY}/admission-server:${GIT_TAG} \
+    -t ${REGISTRY}/admission-server:${VERSION_TAG} \
     --build-arg "COMMIT=${COMMIT}" \
     --build-arg "TAG=${BINARY_TAG}" \
     --platform linux/amd64,linux/arm64 \
-    -t ${REGISTRY}/admission-server:${GIT_TAG} \
-    -t ${REGISTRY}/admission-server:${VERSION_TAG} \
-    --push
+    --push \
+    .
