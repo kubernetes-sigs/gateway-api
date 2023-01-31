03835f526668184be0110067141171f0c4f46cd7
Sajiyah Salat <109643863+Sajiyah-Salat@users.noreply.github.com>
2023-01-31 21:08:34 +0530

    Update apis/v1beta1/shared_types.go
    Co-authored-by: Shane Utt <shane@shaneutt.com>

--- apis/v1beta1/shared_types.go
+++ apis/v1beta1/shared_types.go
@@ -253,9 +253,9 @@ const (
 	// reconciled the route.
 	RouteReasonPending RouteConditionReason = "Pending"
 
-	//This reason is used with the "ResolvedRef" condition when one of
+	// This reason is used with the "ResolvedRef" condition when one of
 	// route has not been accepted because a ParentRef references a Gateway
-	//in another namespace, but no ReferenceGrant allows such a reference.
+	// in another namespace, but no ReferenceGrant allows such a reference.
 	RouteReasonParentRefNotPermitted RouteConditionReason = "ParentRefNotPermitted"
 
 	// This condition indicates whether the controller was able to resolve all
