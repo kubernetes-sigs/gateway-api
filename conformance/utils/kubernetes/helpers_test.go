fc0ad79eacca33d672cd128a757827ed3e03d79b
Arko Dasgupta <arko@tetrate.io>
2023-02-07 15:11:25 -0800

    rm fields not needed
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

--- conformance/utils/kubernetes/helpers_test.go
+++ conformance/utils/kubernetes/helpers_test.go
@@ -4,12 +4,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/gateway-api/apis/v1alpha2"
 	"sigs.k8s.io/gateway-api/apis/v1beta1"
 )
 
-func TestSupportedKindsMatch(t *testing.T) {
+func Test_listenersMatch(t *testing.T) {
 	tests := []struct {
 		name     string
 		expected []v1beta1.ListenerStatus
@@ -17,119 +16,86 @@ func TestSupportedKindsMatch(t *testing.T) {
 		want     bool
 	}{
 		{
-			name: "expected empty and actual is non empty",
+			name: "SupportedKinds: expected empty and actual is non empty",
 			expected: []v1beta1.ListenerStatus{
 				{
-					Name:           v1beta1.SectionName("https"),
 					SupportedKinds: []v1beta1.RouteGroupKind{},
-					Conditions: []metav1.Condition{{
-						Type:   string(v1beta1.ListenerConditionResolvedRefs),
-						Status: metav1.ConditionFalse,
-						Reason: string(v1beta1.ListenerReasonRefNotPermitted),
-					}},
 				},
 			},
 			actual: []v1beta1.ListenerStatus{
 				{
-					Name: v1beta1.SectionName("https"),
-					SupportedKinds: []v1beta1.RouteGroupKind{{
-						Group: (*v1beta1.Group)(&v1beta1.GroupVersion.Group),
-						Kind:  v1beta1.Kind("HTTPRoute"),
-					}},
-					Conditions: []metav1.Condition{{
-						Type:   string(v1beta1.ListenerConditionResolvedRefs),
-						Status: metav1.ConditionFalse,
-						Reason: string(v1beta1.ListenerReasonRefNotPermitted),
-					}},
+					SupportedKinds: []v1beta1.RouteGroupKind{
+						{
+							Group: (*v1beta1.Group)(&v1beta1.GroupVersion.Group),
+							Kind:  v1beta1.Kind("HTTPRoute"),
+						},
+					},
 				},
 			},
 			want: false,
 		},
 		{
-			name: "expected and actual are equal",
+			name: "SupportedKinds: expected and actual are equal",
 			expected: []v1beta1.ListenerStatus{
 				{
-					Name: v1beta1.SectionName("https"),
-					SupportedKinds: []v1beta1.RouteGroupKind{{
-						Group: (*v1beta1.Group)(&v1beta1.GroupVersion.Group),
-						Kind:  v1beta1.Kind("HTTPRoute"),
-					}},
-					Conditions: []metav1.Condition{{
-						Type:   string(v1beta1.ListenerConditionResolvedRefs),
-						Status: metav1.ConditionFalse,
-						Reason: string(v1beta1.ListenerReasonRefNotPermitted),
-					}},
+					SupportedKinds: []v1beta1.RouteGroupKind{
+						{
+							Group: (*v1beta1.Group)(&v1beta1.GroupVersion.Group),
+							Kind:  v1beta1.Kind("HTTPRoute"),
+						},
+					},
 				},
 			},
 			actual: []v1beta1.ListenerStatus{
 				{
-					Name: v1beta1.SectionName("https"),
-					SupportedKinds: []v1beta1.RouteGroupKind{{
-						Group: (*v1beta1.Group)(&v1beta1.GroupVersion.Group),
-						Kind:  v1beta1.Kind("HTTPRoute"),
-					}},
-					Conditions: []metav1.Condition{{
-						Type:   string(v1beta1.ListenerConditionResolvedRefs),
-						Status: metav1.ConditionFalse,
-						Reason: string(v1beta1.ListenerReasonRefNotPermitted),
-					}},
+					SupportedKinds: []v1beta1.RouteGroupKind{
+						{
+							Group: (*v1beta1.Group)(&v1beta1.GroupVersion.Group),
+							Kind:  v1beta1.Kind("HTTPRoute"),
+						},
+					},
 				},
 			},
 			want: true,
 		},
 		{
-			name: "expected kind not found in actual",
+			name: "SupportedKinds: expected kind not found in actual",
 			expected: []v1beta1.ListenerStatus{
 				{
-					Name: v1beta1.SectionName("https"),
-					SupportedKinds: []v1beta1.RouteGroupKind{{
-						Group: (*v1beta1.Group)(&v1beta1.GroupVersion.Group),
-						Kind:  v1beta1.Kind("HTTPRoute"),
-					}},
-					Conditions: []metav1.Condition{{
-						Type:   string(v1beta1.ListenerConditionResolvedRefs),
-						Status: metav1.ConditionFalse,
-						Reason: string(v1beta1.ListenerReasonRefNotPermitted),
-					}},
+					SupportedKinds: []v1beta1.RouteGroupKind{
+						{
+							Group: (*v1beta1.Group)(&v1beta1.GroupVersion.Group),
+							Kind:  v1beta1.Kind("HTTPRoute"),
+						},
+					},
 				},
 			},
 			actual: []v1beta1.ListenerStatus{
 				{
-					Name: v1beta1.SectionName("https"),
 					SupportedKinds: []v1beta1.RouteGroupKind{
 						{
 							Group: (*v1beta1.Group)(&v1alpha2.GroupVersion.Group),
 							Kind:  v1beta1.Kind("GRPCRoute"),
 						},
 					},
-					Conditions: []metav1.Condition{{
-						Type:   string(v1beta1.ListenerConditionResolvedRefs),
-						Status: metav1.ConditionFalse,
-						Reason: string(v1beta1.ListenerReasonRefNotPermitted),
-					}},
 				},
 			},
 			want: false,
 		},
 		{
-			name: "expected is a subset of actual",
+			name: "SupportedKinds: expected is a subset of actual",
 			expected: []v1beta1.ListenerStatus{
 				{
-					Name: v1beta1.SectionName("https"),
-					SupportedKinds: []v1beta1.RouteGroupKind{{
-						Group: (*v1beta1.Group)(&v1beta1.GroupVersion.Group),
-						Kind:  v1beta1.Kind("HTTPRoute"),
-					}},
-					Conditions: []metav1.Condition{{
-						Type:   string(v1beta1.ListenerConditionResolvedRefs),
-						Status: metav1.ConditionFalse,
-						Reason: string(v1beta1.ListenerReasonRefNotPermitted),
-					}},
+					SupportedKinds: []v1beta1.RouteGroupKind{
+						{
+							Group: (*v1beta1.Group)(&v1beta1.GroupVersion.Group),
+							Kind:  v1beta1.Kind("HTTPRoute"),
+						},
+					},
 				},
 			},
 			actual: []v1beta1.ListenerStatus{
 				{
-					Name: v1beta1.SectionName("https"),
 					SupportedKinds: []v1beta1.RouteGroupKind{
 						{
 							Group: (*v1beta1.Group)(&v1alpha2.GroupVersion.Group),
@@ -140,11 +106,6 @@ func TestSupportedKindsMatch(t *testing.T) {
 							Kind:  v1beta1.Kind("HTTPRoute"),
 						},
 					},
-					Conditions: []metav1.Condition{{
-						Type:   string(v1beta1.ListenerConditionResolvedRefs),
-						Status: metav1.ConditionFalse,
-						Reason: string(v1beta1.ListenerReasonRefNotPermitted),
-					}},
 				},
 			},
 			want: true,
