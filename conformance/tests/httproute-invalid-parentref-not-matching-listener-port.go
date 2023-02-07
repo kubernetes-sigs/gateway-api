ac4d9da59044eb72039dd67548e647e89e903298
Shane Utt <shaneutt@linux.com>
2023-02-07 13:40:34 -0500

    Revert "Merge pull request #1694 from mlavacca/parentref-not-permitted-conformance"
    This reverts commit 419ce8501310c4823ecf41aa284e35ba13d33b40.

--- conformance/tests/httproute-invalid-parentref-not-matching-listener-port.go
+++ conformance/tests/httproute-invalid-parentref-not-matching-listener-port.go
@@ -42,13 +42,13 @@ var HTTPRouteInvalidParentRefNotMatchingListenerPort = suite.ConformanceTest{
 
 		// The Route must have an Accepted Condition with a NoMatchingParent Reason.
 		t.Run("HTTPRoute with no matching port in ParentRef has an Accepted Condition with status False and Reason NoMatchingParent", func(t *testing.T) {
-			acceptedCond := metav1.Condition{
+			resolvedRefsCond := metav1.Condition{
 				Type:   string(v1beta1.RouteConditionAccepted),
 				Status: metav1.ConditionFalse,
 				Reason: string(v1beta1.RouteReasonNoMatchingParent),
 			}
 
-			kubernetes.HTTPRouteMustHaveCondition(t, suite.Client, suite.TimeoutConfig, routeNN, gwNN, acceptedCond)
+			kubernetes.HTTPRouteMustHaveCondition(t, suite.Client, suite.TimeoutConfig, routeNN, gwNN, resolvedRefsCond)
 		})
 
 		t.Run("Route should not have Parents accepted in status", func(t *testing.T) {
