d01771081cd0f6bc1e8992eeab69d4f200de6663
dprotaso <dprotaso@gmail.com>
2023-10-12 16:50:57 -0400

    add verify script for docker image building

---
+++ hack/verify-docker-build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright 2023 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")/..
+
+echo "Verifying docker images"
+
+docker buildx rm ${BUILDX_CONTEXT} || true
+docker buildx create --use --name ${BUILDX_CONTEXT} --platform "${BUILDX_PLATFORMS}"
+
+VERIFY=true ./hack/build-and-push.sh
+
