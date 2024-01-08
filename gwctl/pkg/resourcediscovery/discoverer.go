14756595788ffb46e0033524207771f595122e12
Gaurav Ghildiyal <gauravkg@google.com>
2024-01-07 21:36:59 -0800

    Add resourcediscovery package to help discover and model Gateway API resource relationships

---
+++ gwctl/pkg/resourcediscovery/discoverer.go
@@ -0,0 +1,385 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resourcediscovery
+
+import (
+	"context"
+	"fmt"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
+	"sigs.k8s.io/gateway-api/gwctl/pkg/common"
+	"sigs.k8s.io/gateway-api/gwctl/pkg/policymanager"
+	"sigs.k8s.io/gateway-api/gwctl/pkg/relations"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	apimachinerytypes "k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/discovery"
+	"k8s.io/klog/v2"
+	"k8s.io/utils/strings/slices"
+)
+
+// Filter struct defines parameters for filtering resources
+type Filter struct {
+	Namespace    string
+	Name         string
+	Labels       map[string]string
+	ResourceType string
+}
+
+// Discoverer orchestrates the discovery of resources and their associated
+// policies, building a model of interconnected resources.
+//
+// TODO: Optimization Task: Implement a heuristic within each discovery function
+// to intelligently choose between:
+//   - Single API calls for efficient bulk fetching when appropriate.
+//   - Multiple API calls for targeted retrieval when necessary.
+type Discoverer struct {
+	K8sClients    *common.K8sClients
+	PolicyManager *policymanager.PolicyManager
+}
+
+// DiscoverResourcesForGatewayClass discovers resources related to a
+// GatewayClass.
+func (d Discoverer) DiscoverResourcesForGatewayClass(filter Filter) (*ResourceModel, error) {
+	ctx := context.Background()
+	resourceModel := &ResourceModel{}
+
+	gatewayClasses, err := fetchGatewayClasses(ctx, d.K8sClients, filter)
+	if err != nil {
+		return resourceModel, err
+	}
+	resourceModel.addGatewayClasses(gatewayClasses...)
+
+	d.discoverPolicies(ctx, resourceModel)
+
+	return resourceModel, nil
+}
+
+// DiscoverResourcesForGateway discovers resources related to a Gateway.
+func (d Discoverer) DiscoverResourcesForGateway(filter Filter) (*ResourceModel, error) {
+	ctx := context.Background()
+	resourceModel := &ResourceModel{}
+
+	gateways, err := fetchGateways(ctx, d.K8sClients, filter)
+	if err != nil {
+		return resourceModel, err
+	}
+	resourceModel.addGateways(gateways...)
+
+	d.discoverGatewayClassesFromGateways(ctx, resourceModel)
+	d.discoverNamespaces(ctx, resourceModel)
+	d.discoverPolicies(ctx, resourceModel)
+
+	resourceModel.calculateEffectivePolicies()
+
+	return resourceModel, nil
+}
+
+// DiscoverResourcesForHTTPRoute discovers resources related to an HTTPRoute.
+func (d Discoverer) DiscoverResourcesForHTTPRoute(filter Filter) (*ResourceModel, error) {
+	ctx := context.Background()
+	resourceModel := &ResourceModel{}
+
+	httpRoutes, err := fetchHTTPRoutes(ctx, d.K8sClients, filter)
+	if err != nil {
+		return resourceModel, err
+	}
+	resourceModel.addHTTPRoutes(httpRoutes...)
+
+	d.discoverGatewaysFromHTTPRoutes(ctx, resourceModel)
+	d.discoverGatewayClassesFromGateways(ctx, resourceModel)
+	d.discoverNamespaces(ctx, resourceModel)
+	d.discoverPolicies(ctx, resourceModel)
+
+	resourceModel.calculateEffectivePolicies()
+
+	return resourceModel, nil
+}
+
+// DiscoverResourcesForBackend discovers resources related to a Backend.
+func (d Discoverer) DiscoverResourcesForBackend(filter Filter) (*ResourceModel, error) {
+	ctx := context.Background()
+	resourceModel := &ResourceModel{}
+
+	backends, err := fetchBackends(ctx, d.K8sClients, filter)
+	if err != nil {
+		return resourceModel, err
+	}
+	resourceModel.addBackends(backends...)
+
+	d.discoverHTTPRoutesFromBackends(ctx, resourceModel)
+	d.discoverGatewaysFromHTTPRoutes(ctx, resourceModel)
+	d.discoverGatewayClassesFromGateways(ctx, resourceModel)
+	d.discoverNamespaces(ctx, resourceModel)
+	d.discoverPolicies(ctx, resourceModel)
+
+	resourceModel.calculateEffectivePolicies()
+
+	return resourceModel, nil
+}
+
+// discoverGatewayClassesFromGateways will add GatewayClasses associated with
+// Gateways in the resourceModel.
+func (d Discoverer) discoverGatewayClassesFromGateways(ctx context.Context, resourceModel *ResourceModel) {
+	gatewayClasses, err := fetchGatewayClasses(ctx, d.K8sClients, Filter{ /* all GatewayClasses */ })
+	if err != nil {
+		klog.V(1).ErrorS(err, "Failed to list all GatewayClasses")
+	}
+
+	// Build temporary index for GatewayClasses
+	gatewayClassesByID := make(map[gatewayClassID]gatewayv1.GatewayClass)
+	for _, gatewayClass := range gatewayClasses {
+		gatewayClassesByID[GatewayClassID(gatewayClass.GetName())] = gatewayClass
+	}
+
+	for gatewayID, gatewayNode := range resourceModel.Gateways {
+		gatewayClassID := GatewayClassID(relations.FindGatewayClassNameForGateway(*gatewayNode.Gateway))
+		gatewayClass, ok := gatewayClassesByID[gatewayClassID]
+		if !ok {
+			klog.V(1).ErrorS(nil, "GatewayClass referenced in Gateway does not exist",
+				"gateway", gatewayNode.Gateway.GetNamespace()+"/"+gatewayNode.Gateway.GetName(),
+			)
+			continue
+		}
+
+		resourceModel.addGatewayClasses(gatewayClass)
+		resourceModel.connectGatewayWithGatewayClass(gatewayID, gatewayClassID)
+	}
+}
+
+// discoverGatewaysFromHTTPRoutes will add Gateways associated with HTTPRoutes
+// in the resourceModel.
+func (d Discoverer) discoverGatewaysFromHTTPRoutes(ctx context.Context, resourceModel *ResourceModel) {
+	// Visit all gateways corresponding to the httpRoutes
+	for _, httpRouteNode := range resourceModel.HTTPRoutes {
+		for _, gatewayRef := range relations.FindGatewayRefsForHTTPRoute(*httpRouteNode.HTTPRoute) {
+			// Check if Gateway already exists in the resourceModel.
+			if _, ok := resourceModel.Gateways[GatewayID(gatewayRef.Namespace, gatewayRef.Name)]; ok {
+				// Gateway already exists in the resourceModel, skip re-fetching.
+				continue
+			}
+
+			// Gateway doesn't already exist so fetch and add it to the resourceModel.
+			gateways, err := fetchGateways(ctx, d.K8sClients, Filter{Namespace: gatewayRef.Namespace, Name: gatewayRef.Name})
+			if err != nil {
+				klog.V(1).ErrorS(err, "Gateway referenced by HTTPRoute not found",
+					"gateway", gatewayRef.String(),
+					"httproute", httpRouteNode.HTTPRoute.GetNamespace()+"/"+httpRouteNode.HTTPRoute.GetName(),
+				)
+				continue
+			}
+			resourceModel.addGateways(gateways[0])
+		}
+	}
+
+	// Connect gatewayd with httproutes.
+	for httpRouteID, httpRouteNode := range resourceModel.HTTPRoutes {
+		for _, gatewayRef := range relations.FindGatewayRefsForHTTPRoute(*httpRouteNode.HTTPRoute) {
+			resourceModel.connectHTTPRouteWithGateway(httpRouteID, GatewayID(gatewayRef.Namespace, gatewayRef.Name))
+		}
+	}
+}
+
+// discoverHTTPRoutesFromBackends will add HTTPRoutes that reference any Backend
+// present in resourceModel.
+func (d Discoverer) discoverHTTPRoutesFromBackends(ctx context.Context, resourceModel *ResourceModel) {
+	httpRoutes, err := fetchHTTPRoutes(ctx, d.K8sClients, Filter{ /* all HTTPRoutes */ })
+	if err != nil {
+		klog.V(1).ErrorS(err, "Failed to list all HTTPRoutes")
+	}
+
+	for _, httpRoute := range httpRoutes {
+		var found bool
+		for _, backendRef := range relations.FindBackendRefsForHTTPRoute(httpRoute) {
+			backendID := BackendID(backendRef.Group, backendRef.Kind, backendRef.Namespace, backendRef.Name)
+			_, ok := resourceModel.Backends[backendID]
+			if !ok {
+				continue
+			}
+			found = true
+
+			resourceModel.addHTTPRoutes(httpRoute)
+			resourceModel.connectHTTPRouteWithBackend(HTTPRouteID(httpRoute.GetNamespace(), httpRoute.GetName()), backendID)
+		}
+		if !found {
+			klog.V(1).InfoS("Skipping HTTPRoute since it does not reference any required Backend",
+				"httpRoute", httpRoute.GetNamespace()+"/"+httpRoute.GetName(),
+			)
+		}
+	}
+}
+
+// discoverNamespaces discovers Namespaces for resources that exist in the
+// resourceModel.
+func (d Discoverer) discoverNamespaces(ctx context.Context, resourceModel *ResourceModel) {
+	for gatewayID, gatewayNode := range resourceModel.Gateways {
+		resourceModel.addNamespace(gatewayNode.Gateway.GetNamespace())
+		resourceModel.connectGatewayWithNamespace(gatewayID, NamespaceID(gatewayNode.Gateway.GetNamespace()))
+	}
+	for httpRouteID, httpRouteNode := range resourceModel.HTTPRoutes {
+		resourceModel.addNamespace(httpRouteNode.HTTPRoute.GetNamespace())
+		resourceModel.connectHTTPRouteWithNamespace(httpRouteID, NamespaceID(httpRouteNode.HTTPRoute.GetNamespace()))
+	}
+	for backendID, backendNode := range resourceModel.Backends {
+		resourceModel.addNamespace(backendNode.Backend.GetNamespace())
+		resourceModel.connectBackendWithNamespace(backendID, NamespaceID(backendNode.Backend.GetNamespace()))
+	}
+}
+
+// discoverPolicies discovers Policies for resources that exist in the
+// resourceModel.
+func (d Discoverer) discoverPolicies(ctx context.Context, resourceModel *ResourceModel) {
+	resourceModel.addPolicyIfTargetExists(d.PolicyManager.GetPolicies()...)
+}
+
+// fetchGatewayClasses fetches GatewayClasses based on a filter.
+func fetchGatewayClasses(ctx context.Context, k8sClients *common.K8sClients, filter Filter) ([]gatewayv1.GatewayClass, error) {
+	if filter.Name != "" {
+		// Use Get call.
+		gatewayClass := &gatewayv1.GatewayClass{}
+		nn := apimachinerytypes.NamespacedName{Name: filter.Name}
+		if err := k8sClients.Client.Get(ctx, nn, gatewayClass); err != nil {
+			return []gatewayv1.GatewayClass{}, err
+		}
+
+		return []gatewayv1.GatewayClass{*gatewayClass}, nil
+	}
+
+	// Use List call.
+	options := &client.ListOptions{
+		Namespace:     filter.Namespace,
+		LabelSelector: labels.SelectorFromSet(filter.Labels),
+	}
+	gatewayClassList := &gatewayv1.GatewayClassList{}
+	if err := k8sClients.Client.List(ctx, gatewayClassList, options); err != nil {
+		return []gatewayv1.GatewayClass{}, err
+	}
+
+	return gatewayClassList.Items, nil
+}
+
+// fetchGateways fetches Gateways based on a filter.
+func fetchGateways(ctx context.Context, k8sClients *common.K8sClients, filter Filter) ([]gatewayv1.Gateway, error) {
+	if filter.Name != "" {
+		// Use Get call.
+		gateway := &gatewayv1.Gateway{}
+		nn := apimachinerytypes.NamespacedName{Namespace: filter.Namespace, Name: filter.Name}
+		if err := k8sClients.Client.Get(ctx, nn, gateway); err != nil {
+			return []gatewayv1.Gateway{}, err
+		}
+
+		return []gatewayv1.Gateway{*gateway}, nil
+	}
+
+	// Use List call.
+	options := &client.ListOptions{
+		Namespace:     filter.Namespace,
+		LabelSelector: labels.SelectorFromSet(filter.Labels),
+	}
+	gatewayList := &gatewayv1.GatewayList{}
+	if err := k8sClients.Client.List(ctx, gatewayList, options); err != nil {
+		return []gatewayv1.Gateway{}, err
+	}
+
+	return gatewayList.Items, nil
+}
+
+// fetchHTTPRoutes fetches HTTPRoutes based on a filter.
+func fetchHTTPRoutes(ctx context.Context, k8sClients *common.K8sClients, filter Filter) ([]gatewayv1.HTTPRoute, error) {
+	if filter.Name != "" {
+		// Use Get call.
+		httpRoute := &gatewayv1.HTTPRoute{}
+		nn := apimachinerytypes.NamespacedName{Namespace: filter.Namespace, Name: filter.Name}
+		if err := k8sClients.Client.Get(ctx, nn, httpRoute); err != nil {
+			return []gatewayv1.HTTPRoute{}, err
+		}
+
+		return []gatewayv1.HTTPRoute{*httpRoute}, nil
+	}
+
+	// Use List call.
+	options := &client.ListOptions{
+		Namespace:     filter.Namespace,
+		LabelSelector: labels.SelectorFromSet(filter.Labels),
+	}
+	httpRouteList := &gatewayv1.HTTPRouteList{}
+	if err := k8sClients.Client.List(ctx, httpRouteList, options); err != nil {
+		return []gatewayv1.HTTPRoute{}, err
+	}
+
+	return httpRouteList.Items, nil
+}
+
+// fetchBackends fetches Backends based on a filter.
+func fetchBackends(ctx context.Context, k8sClients *common.K8sClients, filter Filter) ([]unstructured.Unstructured, error) {
+	apiResource, err := apiResourceFromResourceType(filter.ResourceType, k8sClients.DiscoveryClient)
+	if err != nil {
+		return nil, err
+	}
+	gvr := schema.GroupVersionResource{
+		Group:    apiResource.Group,
+		Version:  apiResource.Version,
+		Resource: apiResource.Name,
+	}
+
+	listOptions := metav1.ListOptions{}
+	if filter.Name != "" {
+		listOptions.FieldSelector = fields.OneTermEqualSelector("metadata.name", filter.Name).String()
+	}
+
+	var backendsList *unstructured.UnstructuredList
+	if apiResource.Namespaced {
+		backendsList, err = k8sClients.DC.Resource(gvr).Namespace(filter.Namespace).List(ctx, listOptions)
+	} else {
+		backendsList, err = k8sClients.DC.Resource(gvr).List(ctx, listOptions)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return backendsList.Items, nil
+}
+
+func apiResourceFromResourceType(resourceType string, discoveryClient discovery.DiscoveryInterface) (metav1.APIResource, error) {
+	resourceGroups, err := discoveryClient.ServerPreferredResources()
+	if err != nil {
+		return metav1.APIResource{}, err
+	}
+	for _, resourceGroup := range resourceGroups {
+		gv, err := schema.ParseGroupVersion(resourceGroup.GroupVersion)
+		if err != nil {
+			return metav1.APIResource{}, err
+		}
+		for _, resource := range resourceGroup.APIResources {
+			var choices []string
+			choices = append(choices, resource.Kind)
+			choices = append(choices, resource.Name)
+			choices = append(choices, resource.ShortNames...)
+			choices = append(choices, resource.SingularName)
+			if slices.Contains(choices, resourceType) {
+				resource.Version = gv.Version
+				return resource, nil
+			}
+		}
+	}
+	return metav1.APIResource{}, fmt.Errorf("GVR for %v not found in discovery", resourceType)
+}
---
+++ gwctl/pkg/resourcediscovery/nodes.go
@@ -0,0 +1,327 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resourcediscovery
+
+import (
+	"fmt"
+	"strings"
+
+	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
+	"sigs.k8s.io/gateway-api/gwctl/pkg/policymanager"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/klog/v2"
+)
+
+// resourceID defines a type to represent unique IDs for a resource.
+type resourceID struct {
+	Group     string
+	Kind      string
+	Namespace string
+	Name      string
+}
+
+type gatewayClassID resourceID
+type namespaceID resourceID
+type gatewayID resourceID
+type httpRouteID resourceID
+type backendID resourceID
+type policyID resourceID
+
+// GatewayClassID returns an ID for a GatewayClass.
+func GatewayClassID(gatewayClassName string) gatewayClassID {
+	return gatewayClassID(resourceID{Name: gatewayClassName})
+}
+
+// NamespaceID returns an ID for a Namespace.
+func NamespaceID(namespaceName string) namespaceID {
+	if namespaceName == "" {
+		namespaceName = metav1.NamespaceDefault
+	}
+	return namespaceID(resourceID{Name: namespaceName})
+}
+
+// GatewayID returns an ID for a Gateway.
+func GatewayID(namespace, name string) gatewayID {
+	if namespace == "" {
+		namespace = metav1.NamespaceDefault
+	}
+	return gatewayID(resourceID{Namespace: namespace, Name: name})
+}
+
+// HTTPRouteID returns an ID for a HTTPRoute.
+func HTTPRouteID(namespace, name string) httpRouteID {
+	if namespace == "" {
+		namespace = metav1.NamespaceDefault
+	}
+	return httpRouteID(resourceID{Namespace: namespace, Name: name})
+}
+
+// BackendID returns an ID for a Backend.
+func BackendID(group, kind, namespace, name string) backendID {
+	return backendID(resourceID{
+		Group:     strings.ToLower(group),
+		Kind:      strings.ToLower(kind),
+		Namespace: namespace,
+		Name:      name,
+	})
+}
+
+// BackendIDForService returns an ID for a Backend which contains an underlying
+// Service type.
+func BackendIDForService(namespace, name string) backendID {
+	return BackendID("", "service", namespace, name)
+}
+
+// PolicyID returns an ID for a Policy.
+func PolicyID(group, kind, namespace, name string) policyID {
+	return policyID(resourceID{
+		Group:     strings.ToLower(group),
+		Kind:      strings.ToLower(kind),
+		Namespace: namespace,
+		Name:      name,
+	})
+}
+
+// MarshalText is used to implement encoding.TextMarshaler interface for
+// gatewayID.
+func (g gatewayID) MarshalText() ([]byte, error) {
+	return []byte(fmt.Sprintf("%v/%v", g.Namespace, g.Name)), nil
+}
+
+// GatewayClassNode models the relationships and dependencies of a GatewayClass
+// resource.
+type GatewayClassNode struct {
+	// GatewayClass references the actual GatewayClass resource.
+	GatewayClass *gatewayv1.GatewayClass
+
+	// Gateways tracks Gateways that are configured to use this GatewayClass.
+	Gateways map[gatewayID]*GatewayNode
+	// Policies stores Policies that directly apply to this GatewayClass.
+	Policies map[policyID]*PolicyNode
+}
+
+func NewGatewayClassNode(gatewayClass *gatewayv1.GatewayClass) *GatewayClassNode {
+	return &GatewayClassNode{
+		GatewayClass: gatewayClass,
+		Gateways:     make(map[gatewayID]*GatewayNode),
+		Policies:     make(map[policyID]*PolicyNode),
+	}
+}
+
+func (g *GatewayClassNode) ID() gatewayClassID {
+	if g.GatewayClass == nil {
+		klog.V(0).ErrorS(nil, "returning empty ID since GatewayClass is nil")
+		return gatewayClassID(resourceID{})
+	}
+	return GatewayClassID(g.GatewayClass.GetName())
+}
+
+// GatewayNode models the relationships and dependencies of a Gateway resource.
+type GatewayNode struct {
+	// Gateway references the actual Gateway resource.
+	Gateway *gatewayv1.Gateway
+
+	// Namespace is the namespace of the Gateway.
+	Namespace *NamespaceNode
+	// GatewayClass tracks the GatewayClass for this Gateway.
+	GatewayClass *GatewayClassNode
+	// HTTPRoutes stores HTTPRoutes attached to this Gateway.
+	HTTPRoutes map[httpRouteID]*HTTPRouteNode
+	// Policies stores Policies directly applied to the Gateway.
+	Policies map[policyID]*PolicyNode
+	// EffectivePolicies reflects the effective policies applicable to this Gateway,
+	// considering inheritance and hierarchy.
+	EffectivePolicies map[policymanager.PolicyCrdID]policymanager.Policy
+}
+
+func NewGatewayNode(gateway *gatewayv1.Gateway) *GatewayNode {
+	return &GatewayNode{
+		Gateway:           gateway,
+		HTTPRoutes:        make(map[httpRouteID]*HTTPRouteNode),
+		Policies:          make(map[policyID]*PolicyNode),
+		EffectivePolicies: make(map[policymanager.PolicyCrdID]policymanager.Policy),
+	}
+}
+
+func (g *GatewayNode) ID() gatewayID {
+	if g.Gateway == nil {
+		klog.V(0).ErrorS(nil, "returning empty ID since Gateway is nil")
+		return gatewayID(resourceID{})
+	}
+	return GatewayID(g.Gateway.GetNamespace(), g.Gateway.GetName())
+}
+
+// HTTPRouteNode models the relationships and dependencies of an HTTPRoute
+// resource.
+type HTTPRouteNode struct {
+	// HTTPRoute references the actual HTTPRoute resource.
+	HTTPRoute *gatewayv1.HTTPRoute
+
+	// Namespace is the namespace of the HTTPRoute.
+	Namespace *NamespaceNode
+	// Gateways stores Gateways whhich this HTTPRoute is attached to.
+	Gateways map[gatewayID]*GatewayNode
+	// Backends lists Backends serving as target endpoints for traffic through
+	// this route.
+	Backends map[backendID]*BackendNode
+	// Policies stores Policies directly applied to the HTTPRoute.
+	Policies map[policyID]*PolicyNode
+	// EffectivePolicies reflects the effective policies applicable to this
+	// HTTPRoute, mapped per Gateway for context-specific enforcement.
+	EffectivePolicies map[gatewayID]map[policymanager.PolicyCrdID]policymanager.Policy
+}
+
+func NewHTTPRouteNode(httpRoute *gatewayv1.HTTPRoute) *HTTPRouteNode {
+	return &HTTPRouteNode{
+		HTTPRoute:         httpRoute,
+		Gateways:          make(map[gatewayID]*GatewayNode),
+		Backends:          make(map[backendID]*BackendNode),
+		Policies:          make(map[policyID]*PolicyNode),
+		EffectivePolicies: make(map[gatewayID]map[policymanager.PolicyCrdID]policymanager.Policy),
+	}
+}
+
+func (h *HTTPRouteNode) ID() httpRouteID {
+	if h.HTTPRoute == nil {
+		klog.V(0).ErrorS(nil, "returning empty ID since HTTPRoute is nil")
+		return httpRouteID(resourceID{})
+	}
+	return HTTPRouteID(h.HTTPRoute.GetNamespace(), h.HTTPRoute.GetName())
+}
+
+// BackendNode models the relationships and dependencies of a Backend resource,
+// representing the ultimate destination for traffic directed by HTTPRoutes. It
+// serves as a generic abstraction, encompassing various underlying resource
+// types that can act as traffic targets, such as Services, ServiceImports, etc.
+type BackendNode struct {
+	// Backend references the actual Backend resource.
+	Backend *unstructured.Unstructured
+
+	// Namespace is the namespace of the Backend.
+	Namespace *NamespaceNode
+	// HTTPRoutes lists HTTPRoutes that reference this Backend as a target.
+	HTTPRoutes map[httpRouteID]*HTTPRouteNode
+	// Policies stores Policies directly applied to the Backend.
+	Policies map[policyID]*PolicyNode
+	// EffectivePolicies reflects the effective policies applicable to this
+	// Backend, mapped per Gateway for context-specific enforcement.
+	EffectivePolicies map[gatewayID]map[policymanager.PolicyCrdID]policymanager.Policy
+}
+
+func NewBackendNode(backend *unstructured.Unstructured) *BackendNode {
+	return &BackendNode{
+		Backend:           backend,
+		HTTPRoutes:        make(map[httpRouteID]*HTTPRouteNode),
+		Policies:          make(map[policyID]*PolicyNode),
+		EffectivePolicies: make(map[gatewayID]map[policymanager.PolicyCrdID]policymanager.Policy),
+	}
+}
+
+func (b *BackendNode) ID() backendID {
+	if b.Backend == nil {
+		klog.V(0).ErrorS(nil, "returning empty ID since Backend is empty")
+		return backendID(resourceID{})
+	}
+	return BackendID(
+		b.Backend.GroupVersionKind().Group,
+		b.Backend.GroupVersionKind().Kind,
+		b.Backend.GetNamespace(),
+		b.Backend.GetName(),
+	)
+}
+
+// HTTPRouteNode models the relationships and dependencies of a Namespace.
+type NamespaceNode struct {
+	// NamespaceName identifies the Namespace.
+	NamespaceName string
+
+	// Gateways lists Gateways deployed within the Namespace.
+	Gateways map[gatewayID]*GatewayNode
+	// HTTPRoutes lists HTTPRoutes configured within the Namespace.
+	HTTPRoutes map[httpRouteID]*HTTPRouteNode
+	// Backends lists Backends residing within the Namespace.
+	Backends map[backendID]*BackendNode
+	// Policies stores Policies directly applied to the Namespace.
+	Policies map[policyID]*PolicyNode
+}
+
+func NewNamespaceNode(namespaceName string) *NamespaceNode {
+	if namespaceName == "" {
+		namespaceName = metav1.NamespaceDefault
+	}
+	return &NamespaceNode{
+		NamespaceName: namespaceName,
+		Gateways:      make(map[gatewayID]*GatewayNode),
+		HTTPRoutes:    make(map[httpRouteID]*HTTPRouteNode),
+		Backends:      make(map[backendID]*BackendNode),
+		Policies:      make(map[policyID]*PolicyNode),
+	}
+}
+
+func (n *NamespaceNode) ID() namespaceID {
+	if n.NamespaceName == "" {
+		klog.V(0).ErrorS(nil, "returning empty ID since Namespace is empty")
+		return namespaceID(resourceID{})
+	}
+	return NamespaceID(n.NamespaceName)
+}
+
+// PolicyNode models the relationships and dependencies of a Policy resource
+type PolicyNode struct {
+	// Policy references the actual Policy resource.
+	Policy *policymanager.Policy
+
+	// Namespace references the Namespace to which the policy is directly
+	// attached. It's nil if the policy is not associated with a specific
+	// namespace.
+	Namespace *NamespaceNode
+	// GatewayClass references the GatewayClassNode to which the policy is
+	// directly attached. It's nil if the policy is not associated with a specific
+	// GatewayClass.
+	GatewayClass *GatewayClassNode
+	// Gateway references the GatewayNode to which the policy is directly
+	// attached. It's nil if the policy is not associated with a specific Gateway.
+	Gateway *GatewayNode
+	// HTTPRoute references the HTTPRouteNode to which the policy is directly
+	// attached. It's nil if the policy is not associated with a specific
+	// HTTPRoute.
+	HTTPRoute *HTTPRouteNode
+	// Backend references the BackendNode to which the policy is directly
+	// attached. It's nil if the policy is not associated with a specific Backend.
+	Backend *BackendNode
+}
+
+func NewPolicyNode(policy *policymanager.Policy) *PolicyNode {
+	return &PolicyNode{
+		Policy: policy,
+	}
+}
+
+func (p *PolicyNode) ID() policyID {
+	if p.Policy == nil {
+		klog.V(0).ErrorS(nil, "returning empty ID since Policy is empty")
+		return policyID(resourceID{})
+	}
+	return PolicyID(
+		p.Policy.Unstructured().GroupVersionKind().Group,
+		p.Policy.Unstructured().GetKind(),
+		p.Policy.Unstructured().GetNamespace(),
+		p.Policy.Unstructured().GetName(),
+	)
+}
