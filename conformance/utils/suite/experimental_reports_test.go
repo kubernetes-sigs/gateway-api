e20f560425232247d47bffb9210d54d0f82a06be
Mattia Lavacca <lavacca.mattia@gmail.com>
2024-02-22 14:29:59 +0100

    feat: conformance report summary field
    Signed-off-by: Mattia Lavacca <lavacca.mattia@gmail.com>

---
+++ conformance/utils/suite/experimental_reports_test.go
@@ -0,0 +1,122 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package suite
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	confv1a1 "sigs.k8s.io/gateway-api/conformance/apis/v1alpha1"
+)
+
+func TestBuildSummary(t *testing.T) {
+	testCases := []struct {
+		name            string
+		report          confv1a1.ProfileReport
+		expectedSummary string
+	}{
+		{
+			name: "core tests failed, no extended tests",
+			report: confv1a1.ProfileReport{
+				Name: string(HTTPConformanceProfileName),
+				Core: confv1a1.Status{
+					Result: confv1a1.Failure,
+					Statistics: confv1a1.Statistics{
+						Passed: 5,
+						Failed: 3,
+					},
+				},
+			},
+			expectedSummary: "Core tests failed with 3 test failures.",
+		},
+		{
+			name: "core tests succedeed, extended tests failed",
+			report: confv1a1.ProfileReport{
+				Name: string(HTTPConformanceProfileName),
+				Core: confv1a1.Status{
+					Result: confv1a1.Success,
+					Statistics: confv1a1.Statistics{
+						Passed: 8,
+					},
+				},
+				Extended: &confv1a1.ExtendedStatus{
+					Status: confv1a1.Status{
+						Result: confv1a1.Failure,
+						Statistics: confv1a1.Statistics{
+							Passed: 2,
+							Failed: 1,
+						},
+					},
+				},
+			},
+			expectedSummary: "Core tests succedeed. Extended tests failed with 1 test failures.",
+		},
+		{
+			name: "core tests partially succedeed, extended tests succedeed",
+			report: confv1a1.ProfileReport{
+				Name: string(HTTPConformanceProfileName),
+				Core: confv1a1.Status{
+					Result: confv1a1.Partial,
+					Statistics: confv1a1.Statistics{
+						Passed:  6,
+						Skipped: 2,
+					},
+				},
+				Extended: &confv1a1.ExtendedStatus{
+					Status: confv1a1.Status{
+						Result: confv1a1.Success,
+						Statistics: confv1a1.Statistics{
+							Passed: 2,
+						},
+					},
+				},
+			},
+			expectedSummary: "Core tests partially succedeed with 2 test skips. Extended tests succedeed.",
+		},
+		{
+			name: "core tests succedeed, extended tests partially succedeed",
+			report: confv1a1.ProfileReport{
+				Name: string(HTTPConformanceProfileName),
+				Core: confv1a1.Status{
+					Result: confv1a1.Success,
+					Statistics: confv1a1.Statistics{
+						Passed: 8,
+					},
+				},
+				Extended: &confv1a1.ExtendedStatus{
+					Status: confv1a1.Status{
+						Result: confv1a1.Partial,
+						Statistics: confv1a1.Statistics{
+							Passed:  2,
+							Skipped: 1,
+						},
+					},
+				},
+			},
+			expectedSummary: "Core tests succedeed. Extended tests partially succedeed with 1 test skips.",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc := tc
+			summary := buildSummary(tc.report)
+			require.Equal(t, tc.expectedSummary, summary)
+		})
+	}
+}
