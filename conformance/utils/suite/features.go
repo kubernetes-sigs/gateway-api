2b6caec307f3b33959a777fccc1897209648f342
dprotaso <dprotaso@gmail.com>
2024-02-26 21:33:35 -0500

    rename feature

--- conformance/utils/suite/features.go
+++ conformance/utils/suite/features.go
@@ -126,7 +126,7 @@ const (
 	SupportHTTPRouteRequestMirror SupportedFeature = "HTTPRouteRequestMirror"
 
 	// This option indicates support for HTTPRoute backend request mirror (extended conformance).
-	SupportHTTPRouteRequestMirrorBackend SupportedFeature = "HTTPRouteRequestMirrorBackend"
+	SupportHTTPRouteBackendRequestMirror SupportedFeature = "HTTPRouteBackendRequestMirror"
 
 	// This option indicates support for multiple RequestMirror filters within the same HTTPRoute rule (extended conformance).
 	SupportHTTPRouteRequestMultipleMirrors SupportedFeature = "HTTPRouteRequestMultipleMirrors"
@@ -154,7 +154,7 @@ var HTTPRouteExtendedFeatures = sets.New(
 	SupportHTTPRouteHostRewrite,
 	SupportHTTPRoutePathRewrite,
 	SupportHTTPRouteRequestMirror,
-	SupportHTTPRouteRequestMirrorBackend,
+	SupportHTTPRouteBackendRequestMirror,
 	SupportHTTPRouteRequestMultipleMirrors,
 	SupportHTTPRouteRequestTimeout,
 	SupportHTTPRouteBackendTimeout,
