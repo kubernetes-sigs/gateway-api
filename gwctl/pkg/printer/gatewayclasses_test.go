b4f0307cc9269e73187300e72979e7e111ab74ab
Jongwoo Han <jongwooo.han@gmail.com>
2024-03-08 16:34:33 +0900

    Implement `gwctl get gatewayclass` (#2847)

--- gwctl/pkg/printer/gatewayclasses_test.go
+++ gwctl/pkg/printer/gatewayclasses_test.go
@@ -19,21 +19,115 @@ package printer
 import (
 	"bytes"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
+
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	testingclock "k8s.io/utils/clock/testing"
+
 	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
 	gatewayv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
-
 	"sigs.k8s.io/gateway-api/gwctl/pkg/cmd/utils"
 	"sigs.k8s.io/gateway-api/gwctl/pkg/common"
 	"sigs.k8s.io/gateway-api/gwctl/pkg/resourcediscovery"
 )
 
+func TestGatewayClassesPrinter_Print(t *testing.T) {
+	fakeClock := testingclock.NewFakeClock(time.Now())
+	objects := []runtime.Object{
+		&gatewayv1.GatewayClass{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "bar-com-internal-gateway-class",
+				CreationTimestamp: metav1.Time{
+					Time: fakeClock.Now().Add(-365 * 24 * time.Hour),
+				},
+			},
+			Spec: gatewayv1.GatewayClassSpec{
+				ControllerName: "bar.baz/internal-gateway-class",
+			},
+			Status: gatewayv1.GatewayClassStatus{
+				Conditions: []metav1.Condition{
+					{
+						Type:   "Accepted",
+						Status: "True",
+					},
+				},
+			},
+		},
+		&gatewayv1.GatewayClass{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "foo-com-external-gateway-class",
+				CreationTimestamp: metav1.Time{
+					Time: fakeClock.Now().Add(-100 * 24 * time.Hour),
+				},
+			},
+			Spec: gatewayv1.GatewayClassSpec{
+				ControllerName: "foo.com/external-gateway-class",
+			},
+			Status: gatewayv1.GatewayClassStatus{
+				Conditions: []metav1.Condition{
+					{
+						Type:   "Accepted",
+						Status: "False",
+					},
+				},
+			},
+		},
+		&gatewayv1.GatewayClass{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "foo-com-internal-gateway-class",
+				CreationTimestamp: metav1.Time{
+					Time: fakeClock.Now().Add(-24 * time.Minute),
+				},
+			},
+			Spec: gatewayv1.GatewayClassSpec{
+				ControllerName: "foo.com/internal-gateway-class",
+			},
+			Status: gatewayv1.GatewayClassStatus{
+				Conditions: []metav1.Condition{
+					{
+						Type:   "Accepted",
+						Status: "Unknown",
+					},
+				},
+			},
+		},
+	}
+
+	params := utils.MustParamsForTest(t, common.MustClientsForTest(t, objects...))
+	discoverer := resourcediscovery.Discoverer{
+		K8sClients:    params.K8sClients,
+		PolicyManager: params.PolicyManager,
+	}
+	resourceModel, err := discoverer.DiscoverResourcesForGatewayClass(resourcediscovery.Filter{})
+	if err != nil {
+		t.Fatalf("Failed to construct resourceModel: %v", resourceModel)
+	}
+
+	gcp := &GatewayClassesPrinter{
+		Out:   params.Out,
+		Clock: fakeClock,
+	}
+	gcp.Print(resourceModel)
+
+	got := params.Out.(*bytes.Buffer).String()
+	want := `
+NAME                            CONTROLLER                      ACCEPTED  AGE
+bar-com-internal-gateway-class  bar.baz/internal-gateway-class  True      365d
+foo-com-external-gateway-class  foo.com/external-gateway-class  False     100d
+foo-com-internal-gateway-class  foo.com/internal-gateway-class  Unknown   24m
+`
+	if diff := cmp.Diff(common.YamlString(want), common.YamlString(got), common.YamlStringTransformer); diff != "" {
+		t.Errorf("Unexpected diff\ngot=\n%v\nwant=\n%v\ndiff (-want +got)=\n%v", got, want, diff)
+	}
+}
+
 func TestGatewayClassesPrinter_PrintDescribeView(t *testing.T) {
+	fakeClock := testingclock.NewFakeClock(time.Now())
 	objects := []runtime.Object{
 		&gatewayv1.GatewayClass{
 			ObjectMeta: metav1.ObjectMeta{
@@ -89,7 +183,8 @@ func TestGatewayClassesPrinter_PrintDescribeView(t *testing.T) {
 	}
 
 	gcp := &GatewayClassesPrinter{
-		Out: params.Out,
+		Out:   params.Out,
+		Clock: fakeClock,
 	}
 	gcp.PrintDescribeView(resourceModel)
 
