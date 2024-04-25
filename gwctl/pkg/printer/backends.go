1c251172e8f2441883516b520f4658e2527b669b
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2024-04-25 05:01:19 -0400

    feat: gwctl get backends support
    Signed-off-by: Yashvardhan Kukreja <yash.kukreja.98@gmail.com>

--- gwctl/pkg/printer/backends.go
+++ gwctl/pkg/printer/backends.go
@@ -20,6 +20,14 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
+	"strings"
+	"text/tabwriter"
+
+	"k8s.io/apimachinery/pkg/util/duration"
+	"k8s.io/utils/clock"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"sigs.k8s.io/yaml"
 
@@ -28,7 +36,91 @@ import (
 )
 
 type BackendsPrinter struct {
-	Out io.Writer
+	Out   io.Writer
+	Clock clock.Clock
+}
+
+func (bp *BackendsPrinter) Print(resourceModel *resourcediscovery.ResourceModel) {
+	tw := tabwriter.NewWriter(bp.Out, 0, 0, 2, ' ', 0)
+	row := []string{"NAMESPACE", "NAME", "TYPE", "REFERRED BY ROUTES", "AGE", "POLICIES"}
+	tw.Write([]byte(strings.Join(row, "\t") + "\n"))
+
+	backendNodes := make([]*resourcediscovery.BackendNode, 0, len(resourceModel.Backends))
+	for _, backendNode := range resourceModel.Backends {
+		backendNodes = append(backendNodes, backendNode)
+	}
+
+	sort.Slice(backendNodes, func(i, j int) bool {
+		if backendNodes[i].Backend.GetNamespace() != backendNodes[j].Backend.GetNamespace() {
+			return backendNodes[i].Backend.GetNamespace() < backendNodes[j].Backend.GetNamespace()
+		}
+		return backendNodes[i].Backend.GetName() < backendNodes[j].Backend.GetName()
+	})
+
+	for _, backendNode := range backendNodes {
+		backend := backendNode.Backend
+
+		parentHttpRoutes := []string{}
+		remainderHttpRoutes := 0
+		associatedDistinctPolicies := map[string]any{}
+
+		httpRouteNodes := make([]*resourcediscovery.HTTPRouteNode, len(backendNode.HTTPRoutes))
+		i := 0
+		for _, node := range backendNode.HTTPRoutes {
+			httpRouteNodes[i] = node
+			i++
+		}
+		sort.Slice(httpRouteNodes, func(i, j int) bool {
+			if httpRouteNodes[i].HTTPRoute.GetNamespace() != httpRouteNodes[j].HTTPRoute.GetNamespace() {
+				return httpRouteNodes[i].HTTPRoute.GetNamespace() < httpRouteNodes[j].HTTPRoute.GetNamespace()
+			}
+			return httpRouteNodes[i].HTTPRoute.GetName() < httpRouteNodes[j].HTTPRoute.GetName()
+		})
+
+		for _, httpRouteNode := range httpRouteNodes {
+			httpRoute := httpRouteNode.HTTPRoute
+
+			if len(parentHttpRoutes) < 2 {
+				namespacedName := client.ObjectKeyFromObject(httpRoute).String()
+				parentHttpRoutes = append(parentHttpRoutes, namespacedName)
+			} else {
+				remainderHttpRoutes += 1
+			}
+
+			// TODO(yashvardhan-kukreja): change this to httpRouteNode.EffectivePolicies once the work around inherited policies is matured
+			for _, policy := range httpRouteNode.Policies {
+				unstructuredPolicy := policy.Policy.Unstructured()
+				key := fmt.Sprintf("%s/%s/%s",
+					unstructuredPolicy.GetKind(), unstructuredPolicy.GetNamespace(), unstructuredPolicy.GetName())
+				associatedDistinctPolicies[key] = true
+			}
+		}
+
+		referredByRoutes := "None"
+		if len(parentHttpRoutes) != 0 {
+			referredByRoutes = strings.Join(parentHttpRoutes, ",")
+			if remainderHttpRoutes != 0 {
+				referredByRoutes += fmt.Sprintf(" + %d more", remainderHttpRoutes)
+			}
+		}
+
+		namespace := backendNode.Backend.GetNamespace()
+		name := backendNode.Backend.GetName()
+		backendType := backendNode.Backend.GetKind()
+		age := duration.HumanDuration(bp.Clock.Since(backend.GetCreationTimestamp().Time))
+		policiesCount := fmt.Sprintf("%d", len(associatedDistinctPolicies))
+
+		row := []string{
+			namespace,
+			name,
+			backendType,
+			referredByRoutes,
+			age,
+			policiesCount,
+		}
+		tw.Write([]byte(strings.Join(row, "\t") + "\n"))
+	}
+	tw.Flush()
 }
 
 type backendDescribeView struct {
