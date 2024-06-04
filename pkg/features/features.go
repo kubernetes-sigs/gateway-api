f6689f57ec6eb7c885217984187b4f7f13b34fa6
Arnaud Tincelin <25585350+arnaud-tincelin@users.noreply.github.com>
2024-06-04 15:28:57 +0200

    HTTPBackendRef Filter HTTPRouteFilterRequestRedirect conformance test

--- pkg/features/features.go
+++ pkg/features/features.go
@@ -150,6 +150,9 @@ const (
 
 	// This option indicates support for HTTPRoute with a backendref with an appProtoocol 'kubernetes.io/ws' (extended conformance)
 	SupportHTTPRouteBackendProtocolWebSocket SupportedFeature = "HTTPRouteBackendProtocolWebSocket"
+
+	// This option indicates support for HTTPRoute filter request redirect (extended conformance)
+	SupportHTTPRouteFilterRequestRedirect SupportedFeature = "HTTPRouteFilterRequestRedirect"
 )
 
 // HTTPRouteExtendedFeatures includes all extended features for HTTPRoute
@@ -172,6 +175,7 @@ var HTTPRouteExtendedFeatures = sets.New(
 	SupportHTTPRouteBackendRequestHeaderModification,
 	SupportHTTPRouteBackendProtocolH2C,
 	SupportHTTPRouteBackendProtocolWebSocket,
+	SupportHTTPRouteFilterRequestRedirect,
 )
 
 // -----------------------------------------------------------------------------
