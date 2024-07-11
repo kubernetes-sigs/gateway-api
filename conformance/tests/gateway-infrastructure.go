81c3f958c7142ad47c1d95311dfd576f85ac1665
Keith Mattix II <keithmattix@microsoft.com>
2024-07-11 18:13:09 +0000

    gofmt
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>

--- conformance/tests/gateway-infrastructure.go
+++ conformance/tests/gateway-infrastructure.go
@@ -8,6 +8,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	v1 "sigs.k8s.io/gateway-api/apis/v1"
 	"sigs.k8s.io/gateway-api/conformance/utils/kubernetes"
