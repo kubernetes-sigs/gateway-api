5529562e4ded2449c55c7ea02fd23b8e06d169a6
Whitney Griffith <whitney.griffith16@gmail.com>
2024-08-13 19:00:43 +0000

    update  test
    Signed-off-by: Whitney Griffith <whitney.griffith16@gmail.com>

--- conformance/tests/backend-tls-policy-targetref-matching-hostname.go
+++ conformance/tests/backend-tls-policy-targetref-matching-hostname.go
@@ -20,8 +20,10 @@ import (
 	"testing"
 
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/gateway-api/conformance/utils/http"
 	"sigs.k8s.io/gateway-api/conformance/utils/kubernetes"
 	"sigs.k8s.io/gateway-api/conformance/utils/suite"
+	"sigs.k8s.io/gateway-api/conformance/utils/tls"
 	"sigs.k8s.io/gateway-api/pkg/features"
 )
 
@@ -46,15 +48,31 @@ var BackendTLSPolicyTargetRefMatchingHostname = suite.ConformanceTest{
 		// 	 deployment: gateway-conformance-infra/tls-backend that serves a cert with common name "www.abc.example.com"
 		//   backend service with a cert: gateway-conformance-infra/tls-backend
 		// 	 backendTLSPolicy: gateway-conformance-infra/tls-backend-tls-policy with CACertificateRef gateway-conformance-infra/backend-tls-checks-certificate
-
+		// 	 service account?
 		ns := "gateway-conformance-infra"
 		routeNN := types.NamespacedName{Name: "tls-backend-route", Namespace: ns}
 		gwNN := types.NamespacedName{Name: "tls-backend-gateway", Namespace: ns}
 		certNN := types.NamespacedName{Name: "backend-tls-checks-certificate", Namespace: ns}
 
 		kubernetes.NamespacesMustBeReady(t, suite.Client, suite.TimeoutConfig, []string{ns})
+		// wait until the specified Gateway has an IP and the specified HTTPRoute is accepted
+		gwAddr := kubernetes.GatewayAndHTTPRoutesMustBeAccepted(t, suite.Client, suite.TimeoutConfig, suite.ControllerName, kubernetes.NewGatewayRef(gwNN), routeNN)
 
-		// wait until the specified Gateway has an IP and the specified HTTPRoute and BackendTLSPolicy is accepted
+		cPem, keyPem, err := GetTLSSecret(suite.Client, certNN)
+		if err != nil {
+			t.Fatalf("unexpected error finding TLS secret: %v", err)
+		}
+		t.Run("Simple TLS request to service should reach tls-backend", func(t *testing.T) {
+			tls.MakeTLSRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gwAddr, cPem, keyPem, "",
+				http.ExpectedResponse{
+					Request:   http.Request{Host: "abc.example.com", Path: "/"},
+					Backend:   "tls-backend",
+					Namespace: "gateway-conformance-infra",
+				})
+
+		})
+
+		// Check cert details received from the backend
 
 	},
 }
