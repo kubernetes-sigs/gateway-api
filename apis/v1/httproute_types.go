cb721ba869557b2de1ec95210ce85eb99b1c7de0
jakebennert <jakebennert@gmail.com>
2024-08-23 19:20:34 -0400

    Update httproute_types.go

--- apis/v1/httproute_types.go
+++ apis/v1/httproute_types.go
@@ -758,7 +758,7 @@ type HTTPRouteFilter struct {
 	//
 	// +optional
 	//
-	// <gateway:experimental:validation:XValidation:message="Only one of percent or fraction may be specified in the HTTPRequestMirrorFilter",rule="!(has(self.percent) && has(self.fraction))">
+	// <gateway:experimental:validation:XValidation:message="Only one of percent or fraction may be specified in HTTPRequestMirrorFilter",rule="!(has(self.percent) && has(self.fraction))">
 	RequestMirror *HTTPRequestMirrorFilter `json:"requestMirror,omitempty"`
 
 	// RequestRedirect defines a schema for a filter that responds to the
