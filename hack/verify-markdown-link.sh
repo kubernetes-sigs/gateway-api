7efcbab40b1cb6e62ba87a663a911bd94fd10058
achrefothmani <achref.othmani@elyadata.com>
2024-11-11 11:16:02 +0100

    Fix boilerplate issue

--- hack/verify-markdown-link.sh
+++ hack/verify-markdown-link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2020 The Kubernetes Authors.
+# Copyright 2014 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,10 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-  
-set -o errexit  
-set -o nounset  
-set -o pipefail  
+
+set -o errexit
+set -o nounset
+set -o pipefail
   
 log_error() {  
     echo "ERROR: $*" 1>&2  
