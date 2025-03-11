c3ed0633feacd8532e9fb52ae13c2f68d3db986c
EyalPazz <67443108+EyalPazz@users.noreply.github.com>
2025-03-11 07:21:47 +0200

    fix: add CORS to `HTTPRouteFilterType` (#3668)
    * fix: add CORS to `HTTPRouteFilterType`
    
    * fix: only add CORS to experimental

--- apis/v1/httproute_types.go
+++ apis/v1/httproute_types.go
@@ -830,6 +830,7 @@ type HTTPRouteFilter struct {
 	//
 	// +unionDiscriminator
 	// +kubebuilder:validation:Enum=RequestHeaderModifier;ResponseHeaderModifier;RequestMirror;RequestRedirect;URLRewrite;ExtensionRef
+	// <gateway:experimental:validation:Enum=RequestHeaderModifier;ResponseHeaderModifier;RequestMirror;RequestRedirect;URLRewrite;ExtensionRef;CORS>
 	Type HTTPRouteFilterType `json:"type"`
 
 	// RequestHeaderModifier defines a schema for a filter that modifies request
