fa7591e22adfe275fb2f5658aca8e96f06e3bb1c
EyalPazz <67443108+EyalPazz@users.noreply.github.com>
2025-03-18 11:49:51 +0200

    fix: validate that a method and a wildcard won't appear together (#3667)
    * fix: validate that a method and a wildcard won't appear together
    
    * fix: use `in` instead of `includes`

--- apis/v1/httproute_types.go
+++ apis/v1/httproute_types.go
@@ -1417,6 +1417,7 @@ type HTTPCORSFilter struct {
 	//
 	// +listType=set
 	// +kubebuilder:validation:MaxItems=9
+	// +kubebuilder:validation:XValidation:message="AllowMethods cannot contain '*' alongside other methods",rule="!('*' in self && self.size() > 1)"
 	AllowMethods []HTTPMethodWithWildcard `json:"allowMethods,omitempty"`
 
 	// AllowHeaders indicates which HTTP request headers are supported for
