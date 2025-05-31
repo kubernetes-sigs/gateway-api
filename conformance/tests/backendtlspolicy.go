ba1992692b24660ebcd3c8c8b3dd6f8165eed6dc
candita <cholman@redhat.com>
2025-05-30 22:10:04 -0400

    Fix the tests for normative BackendTLSPolicy

--- conformance/tests/backendtlspolicy.go
+++ conformance/tests/backendtlspolicy.go
@@ -17,20 +17,14 @@ limitations under the License.
 package tests
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
 	"testing"
-	"time"
 
 	"k8s.io/apimachinery/pkg/types"
 
 	h "sigs.k8s.io/gateway-api/conformance/utils/http"
 	"sigs.k8s.io/gateway-api/conformance/utils/kubernetes"
-	"sigs.k8s.io/gateway-api/conformance/utils/roundtripper"
 	"sigs.k8s.io/gateway-api/conformance/utils/suite"
+	"sigs.k8s.io/gateway-api/conformance/utils/tls"
 	"sigs.k8s.io/gateway-api/pkg/features"
 )
 
@@ -56,69 +50,38 @@ var BackendTLSPolicy = suite.ConformanceTest{
 		kubernetes.HTTPRouteMustHaveResolvedRefsConditionsTrue(t, suite.Client, suite.TimeoutConfig, routeNN, gwNN)
 
 		serverStr := "abc.example.com"
-		headers := make(map[string]string)
-		headers["Host"] = serverStr
 
 		// Verify that the response to a call to /backendTLS will return the matching SNI.
+		certNN := types.NamespacedName{Name: "tls-checks-certificate", Namespace: ns}
+		cPem, keyPem, err := GetTLSSecret(suite.Client, certNN)
+		if err != nil {
+			t.Fatalf("unexpected error finding TLS secret: %v", err)
+		}
 		t.Run("Simple request targeting BackendTLSPolicy should reach infra-backend", func(t *testing.T) {
-			expected := h.ExpectedResponse{
-				Request: h.Request{
-					Headers: headers,
-					Host:    serverStr,
-					Path:    "/backendTLS",
-				},
-				Response: h.Response{StatusCode: 200},
-			}
-			req := h.MakeRequest(t, &expected, gwAddr, "HTTPS", "https")
+			tls.MakeTLSRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gwAddr, cPem, keyPem, serverStr,
+				h.ExpectedResponse{
+					Namespace: ns,
+					Request: h.Request{
+						Host: serverStr,
+						Path: "/backendTLS",
+						SNI:  serverStr,
+					},
+					Response: h.Response{StatusCode: 200},
+				})
+		})
 
-			if found, err := sameSNI(req, serverStr); err != nil || !found {
-				t.Errorf("no SNI found for request: %v", err)
-			}
+		// Verify that expecting the wrong SNI will fail.
+		t.Run("Simple request targeting BackendTLSPolicy and expecting the wrong SNI should fail", func(t *testing.T) {
+			tls.MakeTLSRequestAndExpectFailure(t, suite.RoundTripper, gwAddr, cPem, keyPem, serverStr,
+				h.ExpectedResponse{
+					Namespace: ns,
+					Request: h.Request{
+						Host: serverStr,
+						Path: "/backendTLS",
+						SNI:  "notright",
+					},
+					Response: h.Response{StatusCode: 200},
+				})
 		})
 	},
-}
-
-func sameSNI(request roundtripper.Request, sni string) (bool, error) {
-	client := &http.Client{}
-	assertions := &kubernetes.RequestAssertions{}
-
-	method := "GET"
-	if request.Method != "" {
-		method = request.Method
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
-	defer cancel()
-	req, err := http.NewRequestWithContext(ctx, method, request.URL.String(), nil)
-	if err != nil {
-		return false, fmt.Errorf("error creating request: %v", err)
-	}
-
-	if request.Host != "" {
-		req.Host = request.Host
-	}
-
-	if request.Headers != nil {
-		for name, value := range request.Headers {
-			req.Header.Set(name, value[0])
-		}
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return false, fmt.Errorf("request %v caused an error %v", req, err)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return false, fmt.Errorf("failed to read response body: %v", err)
-	}
-
-	err = json.Unmarshal(body, assertions)
-	if err != nil {
-		return false, fmt.Errorf("unexpected error reading response: %w", err)
-	}
-
-	return assertions.SNI == sni, nil
 }
