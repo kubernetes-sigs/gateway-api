50b20781a00338e06b967bfa171fc6ce18e34295
Mattia Lavacca <lavacca.mattia@gmail.com>
2025-06-24 11:26:12 +0200

    chore(clients): make generate
    Signed-off-by: Mattia Lavacca <lavacca.mattia@gmail.com>

--- pkg/client/informers/externalversions/apis/v1alpha2/udproute.go
+++ pkg/client/informers/externalversions/apis/v1alpha2/udproute.go
@@ -62,13 +62,25 @@ func NewFilteredUDPRouteInformer(client versioned.Interface, namespace string, r
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.GatewayV1alpha2().UDPRoutes(namespace).List(context.TODO(), options)
+				return client.GatewayV1alpha2().UDPRoutes(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.GatewayV1alpha2().UDPRoutes(namespace).Watch(context.TODO(), options)
+				return client.GatewayV1alpha2().UDPRoutes(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.GatewayV1alpha2().UDPRoutes(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.GatewayV1alpha2().UDPRoutes(namespace).Watch(ctx, options)
 			},
 		},
 		&gatewayapiapisv1alpha2.UDPRoute{},
