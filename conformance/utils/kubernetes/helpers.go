615aa3c49b888dc0cea29bf48c0e84e335960077
Rostislav Bobrovsky <rostislavb@google.com>
2025-07-09 13:42:05 +0000

    TLSRoute: Update conformance helpers

--- conformance/utils/kubernetes/helpers.go
+++ conformance/utils/kubernetes/helpers.go
@@ -37,7 +37,6 @@ import (
 
 	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
 	"sigs.k8s.io/gateway-api/apis/v1alpha2"
-	"sigs.k8s.io/gateway-api/apis/v1alpha3"
 	"sigs.k8s.io/gateway-api/conformance/utils/config"
 	"sigs.k8s.io/gateway-api/conformance/utils/tlog"
 )
@@ -631,11 +630,11 @@ func UDPRouteMustHaveParents(t *testing.T, client client.Client, timeoutConfig c
 // TLSRouteMustHaveParents waits for the specified TLSRoute to have parents
 // in status that match the expected parents, and also returns the TLSRoute.
 // This will cause the test to halt if the specified timeout is exceeded.
-func TLSRouteMustHaveParents(t *testing.T, client client.Client, timeoutConfig config.TimeoutConfig, routeName types.NamespacedName, parents []v1alpha2.RouteParentStatus, namespaceRequired bool) v1alpha3.TLSRoute {
+func TLSRouteMustHaveParents(t *testing.T, client client.Client, timeoutConfig config.TimeoutConfig, routeName types.NamespacedName, parents []v1alpha2.RouteParentStatus, namespaceRequired bool) v1alpha2.TLSRoute {
 	t.Helper()
 
 	var actual []gatewayv1.RouteParentStatus
-	var route v1alpha3.TLSRoute
+	var route v1alpha2.TLSRoute
 
 	waitErr := wait.PollUntilContextTimeout(context.Background(), 1*time.Second, timeoutConfig.RouteMustHaveParents, true, func(ctx context.Context) (bool, error) {
 		err := client.Get(ctx, routeName, &route)
@@ -825,7 +824,7 @@ func TLSRouteMustHaveCondition(t *testing.T, client client.Client, timeoutConfig
 	t.Helper()
 
 	waitErr := wait.PollUntilContextTimeout(context.Background(), 1*time.Second, timeoutConfig.TLSRouteMustHaveCondition, true, func(ctx context.Context) (bool, error) {
-		route := &v1alpha3.TLSRoute{}
+		route := &v1alpha2.TLSRoute{}
 		err := client.Get(ctx, routeNN, route)
 		if err != nil {
 			return false, fmt.Errorf("error fetching TLSRoute: %w", err)
