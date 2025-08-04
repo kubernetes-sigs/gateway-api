b6b242839ebd353b25ee41f83d2ce69dd177ea56
Rostislav Bobrovsky <rostislavb@google.com>
2025-08-04 05:57:37 +0200

    TLSRoute: Set MaxItems=1 for rules[] in v1alpha3 (#3971)
    * TLSRoute: Set Rules MaxItems to 1
    
    * TLSRoute: Add omitempty to Rules
    
    * TLSRoute: Rules godoc

--- apis/v1alpha3/tlsroute_types.go
+++ apis/v1alpha3/tlsroute_types.go
@@ -92,13 +92,13 @@ type TLSRouteSpec struct {
 	// +required
 	Hostnames []Hostname `json:"hostnames,omitempty"`
 
-	// Rules are a list of TLS matchers and actions.
+	// Rules are a list of actions.
 	//
 	// +required
 	// +kubebuilder:validation:MinItems=1
-	// +kubebuilder:validation:MaxItems=16
+	// +kubebuilder:validation:MaxItems=1
 	// <gateway:experimental:validation:XValidation:message="Rule name must be unique within the route",rule="self.all(l1, !has(l1.name) || self.exists_one(l2, has(l2.name) && l1.name == l2.name))">
-	Rules []v1alpha2.TLSRouteRule `json:"rules"`
+	Rules []v1alpha2.TLSRouteRule `json:"rules,omitempty"`
 }
 
 // +kubebuilder:object:root=true
