bf81c4542e4691dbca8023b2ea6da76cbc1c61ee
Flynn <flynn@buoyant.io>
2025-08-26 14:24:58 -0400

    chore(build): Tweak update-protos to not rely on GOPATH being in one's environment
    Signed-off-by: Flynn <emissary@flynn.kodachi.com>

--- hack/update-protos.sh
+++ hack/update-protos.sh
@@ -87,7 +87,7 @@ go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
 
 (cd conformance/echo-basic && \
-  export PATH="$PATH:$GOPATH/bin" && \
+  export PATH="$PATH:$(go env GOPATH)/bin" && \
   "${PROTOC_BINARY}" --go_out=grpcechoserver --go_opt=paths=source_relative \
     --go-grpc_out=grpcechoserver --go-grpc_opt=paths=source_relative \
     grpcecho.proto
