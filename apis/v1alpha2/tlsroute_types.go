af8a61b615ed6a895dbd05b77e3359c1d2f4f728
Alexander Stephan <alexander.stephan@sap.com>
2025-12-08 10:57:11 +0000

    Fix typo

--- apis/v1alpha2/tlsroute_types.go
+++ apis/v1alpha2/tlsroute_types.go
@@ -93,8 +93,8 @@ type TLSRouteSpec struct {
 	//
 	// +required
 	// +listType=atomic
-  // +kubebuilder:validation:MaxItems=16
 	// +kubebuilder:validation:MinItems=1
+	// +kubebuilder:validation:MaxItems=16
 	// <gateway:experimental:validation:XValidation:message="Rule name must be unique within the route",rule="self.all(l1, !has(l1.name) || self.exists_one(l2, has(l2.name) && l1.name == l2.name))">
 	Rules []TLSRouteRule `json:"rules"`
 }
