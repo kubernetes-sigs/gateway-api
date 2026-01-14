15a55db21c3e09cebc7f680ce1b55327dc298724
salonichf5 <146118978+salonichf5@users.noreply.github.com>
2026-01-13 17:01:41 -0700

    update mesh conformance tests based on reviews

--- conformance/tests/mesh/grpcroute-request-header-modifier.go
+++ conformance/tests/mesh/grpcroute-request-header-modifier.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 The Kubernetes Authors.
+Copyright 2026 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ limitations under the License.
 package meshtests
 
 import (
+	"maps"
 	"testing"
 
 	"sigs.k8s.io/gateway-api/conformance/utils/echo"
@@ -42,153 +43,107 @@ var MeshGRPCRouteRequestHeaderModifier = suite.ConformanceTest{
 		clientV2 := echo.ConnectToApp(t, s, echo.MeshAppEchoV2)
 
 		testCases := []struct {
-			name            string
-			client          echo.MeshPod
-			headers         map[string]string
-			targetHost      string
-			absentHeaders   []string
-			expectedHeaders map[string]string
+			name             string
+			client           echo.MeshPod
+			matchHeaderValue string
+			targetHost       string
+			headers          map[string]string
+			absentHeaders    []string
+			expectedHeaders  map[string]string
 		}{
 			{
-				name: "Set headers -- X-Header-Set should have the original value",
+				name:             "Set headers -- overwrites existing header",
+				client:           clientV1,
+				targetHost:       "echo-v1",
+				matchHeaderValue: "set",
 				headers: map[string]string{
-					"Some-Other-Header": "this-header-should-be-set",
+					"some-other-header": "this-header-should-be-set",
+					"x-header-set":      "this-value-should-be-overwritten",
 				},
 				expectedHeaders: map[string]string{
-					"Some-Other-Header": "this-header-should-be-set",
-					"X-Header-Set":      "set-overwrites-values",
+					"x-test-case":       "set",
+					"some-other-header": "this-header-should-be-set",
+					"x-header-set":      "set-overwrites-values",
 				},
-				targetHost: "echo-v1",
-				client:     clientV1,
 			},
 			{
-				name: "Set headers -- X-Header-Set should get overwritten with the original value",
+				name:             "Add headers -- appends to existing header",
+				client:           clientV1,
+				targetHost:       "echo-v1",
+				matchHeaderValue: "add",
 				headers: map[string]string{
-					"Some-Other-Header": "this-header-should-be-set",
-					"X-Header-Set":      "this-value-should-be-overwritten",
+					"x-header-add": "this-value-should-be-appended",
 				},
 				expectedHeaders: map[string]string{
-					"Some-Other-Header": "this-header-should-be-set",
-					"X-Header-Set":      "set-overwrites-values",
+					"x-test-case":  "add",
+					"x-header-add": "this-value-should-be-appended,add-appends-values",
 				},
-				targetHost: "echo-v1",
-				client:     clientV1,
 			},
 			{
-				name: "Add headers -- X-Header-Add should have the original value",
+				name:             "Remove headers -- removes headers",
+				client:           clientV1,
+				targetHost:       "echo-v1",
+				matchHeaderValue: "remove",
 				headers: map[string]string{
-					"Some-Other-Header": "this-header-should-be-set",
+					"x-header-remove": "this-should-be-removed",
 				},
+				absentHeaders: []string{"x-header-remove"},
 				expectedHeaders: map[string]string{
-					"Some-Other-Header": "this-header-should-be-set",
-					"X-Header-Add":      "add-appends-values",
+					"x-test-case": "remove",
 				},
-				targetHost: "echo-v1",
-				client:     clientV1,
 			},
 			{
-				name: "X-Header-Add should append the new value to the original value",
+				name:             "Multiple operations -- set, add, and remove headers",
+				client:           clientV2,
+				targetHost:       "echo-v2",
+				matchHeaderValue: "multi",
 				headers: map[string]string{
-					"Some-Other-Header": "this-header-should-be-set",
-					"X-Header-Add":      "this-value-should-be-appended",
+					"x-header-set-2":    "set-header-2",
+					"x-header-add-2":    "add-header-2",
+					"x-header-remove-2": "should-be-removed-2",
 				},
+				absentHeaders: []string{"x-header-remove-1", "x-header-remove-2"},
 				expectedHeaders: map[string]string{
-					"Some-Other-Header": "this-header-should-be-set",
-					"X-Header-Add":      "this-value-should-be-appended,add-appends-values",
+					"x-test-case":    "multi",
+					"x-header-set-1": "header-set-1",
+					"x-header-set-2": "header-set-2",
+					"x-header-add-1": "header-add-1",
+					"x-header-add-2": "add-header-2,header-add-2",
 				},
-				targetHost: "echo-v1",
-				client:     clientV1,
-			},
-			{
-				name: "Remove headers -- X-Header-Remove should be removed",
-				headers: map[string]string{
-					"X-Header-Remove": "this-should-be-removed",
-				},
-				absentHeaders: []string{"X-Header-Remove"},
-				client:        clientV1,
-				targetHost:    "echo-v1",
-			},
-			{
-				name: "Multiple operations - all header operations should be applied",
-				headers: map[string]string{
-					"X-Header-Set-2":    "set-header-2",
-					"X-Header-Add-2":    "add-header-2",
-					"X-Header-Remove-2": "should-be-removed-2",
-					"Some-Other-Header": "another-header-val",
-				},
-				absentHeaders: []string{
-					"X-Header-Remove-1",
-					"X-Header-Remove-2",
-				},
-				expectedHeaders: map[string]string{
-					"X-Header-Set-1":    "header-set-1",
-					"X-Header-Set-2":    "header-set-2",
-					"X-Header-Add-1":    "header-add-1",
-					"X-Header-Add-2":    "add-header-2,header-add-2",
-					"X-Header-Add-3":    "header-add-3",
-					"Some-Other-Header": "another-header-val",
-				},
-				client:     clientV2,
-				targetHost: "echo-v2",
-			},
-			{
-				name: "Case sensitivity check for header names",
-				headers: map[string]string{
-					"x-header-set-1":    "original-set-1",
-					"x-header-add-1":    "existing-add-1",
-					"x-header-remove-1": "should-be-removed-1",
-				},
-				absentHeaders: []string{"X-Header-Remove-1"},
-				expectedHeaders: map[string]string{
-					"X-Header-Set-1": "header-set-1",
-					"X-Header-Set-2": "header-set-2",
-					"X-Header-Add-1": "existing-add-1,header-add-1",
-					"X-Header-Add-2": "header-add-2",
-					"X-Header-Add-3": "header-add-3",
-				},
-				client:     clientV2,
-				targetHost: "echo-v2",
 			},
 		}
 
 		for _, tc := range testCases {
-			tc := tc // capture for parallel execution
 			t.Run(tc.name, func(t *testing.T) {
 				t.Parallel()
 
-				request := http.Request{
+				hdrs := map[string]string{"x-test-case": tc.matchHeaderValue}
+				maps.Copy(hdrs, tc.headers)
+
+				req := http.Request{
 					Protocol: "grpc",
 					Host:     "echo:7070",
-					Path:     "",
-					Headers:  tc.headers,
+					Headers:  hdrs,
 				}
 
-				expected := http.ExpectedResponse{
-					Request: request,
-					Response: http.Response{
-						StatusCode: 200,
+				exp := http.ExpectedResponse{
+					Request: req,
+					ExpectedRequest: &http.ExpectedRequest{
+						Request: http.Request{
+							Protocol: "grpc",
+							Host:     "echo:7070",
+							Headers:  tc.expectedHeaders,
+						},
+						AbsentHeaders: tc.absentHeaders,
 					},
+					Response: http.Response{
+						StatusCodes: []int{200},
+					},
+					Backend:   tc.targetHost,
 					Namespace: "gateway-conformance-mesh",
 				}
 
-				// will remove based on input from Gateway API team
-
-				// expected := http.ExpectedResponse{
-				// 	Request: request,
-				// 	ExpectedRequest: &http.ExpectedRequest{
-				// 		Request: http.Request{
-				// 			Protocol: "grpc",
-				// 			Host:     "echo:7070",
-				// 			Path:     "",
-				// 			Headers:  tc.expectedHeaders,
-				// 		},
-				// 		AbsentHeaders: tc.absentHeaders,
-				// 	},
-				// 	Namespace: "gateway-conformance-mesh",
-				// }
-
-				// Make the request and validate headers are properly modified
-				tc.client.MakeRequestAndExpectEventuallyConsistentResponse(t, expected, s.TimeoutConfig)
+				tc.client.MakeRequestAndExpectEventuallyConsistentResponse(t, exp, s.TimeoutConfig)
 			})
 		}
 	},
