4dc11ed08998e7e559694b926f2b2eee30a7896c
Mark Vander Stel <mvndrstl@gmail.com>
2026-02-27 10:09:35 -0500

    Add new openapi-generator tool to output OpenAPIv2 schema
    The OpenAPI v2 schema that the modelshema tool outputs lacks paths, and
    is formatted specifically to be consumed by other internal tools. Rather
    than mess with it, create a new tool that outputs schemata specifically
    designed to be consumed by third parties.
    
    This new tool supports generating a schema from any CRD definition, and
    will output a schema on the same level as the schema in the main
    Kubernetes project.
    
    Also add a hack script to generate schema files for both channels.

---
+++ tools/openapi-generator/cmd.go
@@ -0,0 +1,46 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	name    string
+	version string
+	output  string
+)
+
+func newCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "openapi-generator [crd file]...",
+		Short: "Convert CRD definitions to an OpenAPI v2 specification",
+		Long: `Read CRD definitions from the specified input files and generate an
+OpenAPI/Swagger v2 spec file in JSON format.`,
+		Args: cobra.MinimumNArgs(1),
+		RunE: run,
+	}
+
+	flags := cmd.Flags()
+
+	flags.StringVarP(&name, "name", "n", "undefined", "Name of the API in the output spec")
+	flags.StringVarP(&version, "version", "v", "undefined", "Version of the API in the output spec")
+	flags.StringVarP(&output, "output", "o", "-", "Output file")
+
+	return cmd
+}
---
+++ tools/openapi-generator/main.go
@@ -0,0 +1,83 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+Take input CRD definitions and output OpenAPI v2 spec. This will work
+with any CRDs, not just Gateway API ones.
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"k8s.io/apiextensions-apiserver/pkg/controller/openapi/builder"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	err := newCommand().Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to generate spec: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(_ *cobra.Command, args []string) error {
+	crds, err := loadCrdFiles(args)
+	if err != nil {
+		return err
+	}
+	if len(crds) == 0 {
+		return fmt.Errorf("no CRDs found")
+	}
+
+	staticSpec := createStaticSpec(name, version)
+
+	specs, err := convertFromCrds(crds)
+	if err != nil {
+		return err
+	}
+
+	mergeSpecs, err := builder.MergeSpecs(staticSpec, specs...)
+	if err != nil {
+		return err
+	}
+
+	json, err := mergeSpecs.MarshalJSON()
+	if err != nil {
+		return err
+	}
+
+	var writer *os.File
+	if output == "-" {
+		writer = os.Stdout
+	} else {
+		writer, err = os.Create(output)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = writer.Write(json)
+	if err != nil {
+		return err
+	}
+	writer.Close()
+	return nil
+}
