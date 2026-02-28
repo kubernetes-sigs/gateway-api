166703447c4d773674131ce8891ca6e4014a0058
samzong <samzong.lu@gmail.com>
2026-02-28 13:28:23 +0800

    fix: propagate context and fix defer leak in DumpEchoLogs
    Signed-off-by: samzong <samzong.lu@gmail.com>

--- conformance/tests/httproute-request-percentage-mirror.go
+++ conformance/tests/httproute-request-percentage-mirror.go
@@ -193,7 +193,7 @@ func testMirroredRequestsDistribution(t *testing.T, suite *suite.ConformanceTest
 
 			tlog.Log(t, "Searching for the mirrored request log")
 			tlog.Logf(t, `Reading "%s/%s" logs`, mirrorPod.Namespace, mirrorPod.Name)
-			logs, err := kubernetes.DumpEchoLogs(mirrorPod.Namespace, mirrorPod.Name, suite.Client, suite.Clientset, timeVal)
+			logs, err := kubernetes.DumpEchoLogs(t.Context(), mirrorPod.Namespace, mirrorPod.Name, suite.Client, suite.Clientset, timeVal)
 			if err != nil {
 				tlog.Logf(t, `Couldn't read "%s/%s" logs: %v`, mirrorPod.Namespace, mirrorPod.Name, err)
 				return false
