ea18957fa7cfb3abedfd7c1f4b7056514be61ccd
zirain <zirain2009@gmail.com>
2026-03-11 11:30:05 +0800

    fix lint
    Signed-off-by: zirain <zirain2009@gmail.com>

--- conformance/tests/httproute-https-listener-detect-misdirected-requests.go
+++ conformance/tests/httproute-https-listener-detect-misdirected-requests.go
@@ -20,10 +20,10 @@ import (
 	"testing"
 
 	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/gateway-api/conformance/utils/roundtripper"
 
 	"sigs.k8s.io/gateway-api/conformance/utils/http"
 	"sigs.k8s.io/gateway-api/conformance/utils/kubernetes"
+	"sigs.k8s.io/gateway-api/conformance/utils/roundtripper"
 	confsuite "sigs.k8s.io/gateway-api/conformance/utils/suite"
 	"sigs.k8s.io/gateway-api/conformance/utils/tls"
 	"sigs.k8s.io/gateway-api/pkg/features"
