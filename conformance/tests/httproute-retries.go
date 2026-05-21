0daa53a3d94403876333b543ab656c2c742c42f8
Norwin Schnyder <norwin.schnyder+github@gmail.com>
2026-05-21 15:58:48 +0200

    test(conformance): add test for HTTPRoute retries with timeouts
    Signed-off-by: Norwin Schnyder <norwin.schnyder+github@gmail.com>

--- conformance/tests/httproute-retries.go
+++ conformance/tests/httproute-retries.go
@@ -185,42 +185,44 @@ var HTTPRouteRetries = confsuite.ConformanceTest{
 			},
 		}
 		for i := range testCases {
-			// Declare tc here to avoid loop variable
-			// reuse issues across parallel tests.
+			// Declare tc here to avoid loop variable reuse issues across parallel tests.
 			tc := testCases[i]
 			t.Run(fmt.Sprintf("%d request to '%s' %s", i, tc.args.path, tc.name), func(t *testing.T) {
 				t.Parallel()
-
-				http.AwaitConvergence(t, suite.TimeoutConfig.RequiredConsecutiveSuccesses, suite.TimeoutConfig.MaxTimeToConsistency, func(elapsed time.Duration) bool {
-					// regenerate UUID on each probe so the retry simulation handler in the echo-basic backend tracks retries independently
-					tc.args.retrySimulationConfig.Set("uuid", uuid.New().String())
-
-					expectedResponse := http.ExpectedResponse{
-						Request: http.Request{
-							Path: tc.args.path + "?" + tc.args.retrySimulationConfig.Encode(),
-						},
-						Response:  tc.want,
-						Backend:   confsuite.InfraBackendServiceNameV3,
-						Namespace: ns,
-					}
-
-					req := http.MakeRequest(t, &expectedResponse, gwAddr, "HTTP", "http")
-
-					cReq, cRes, err := suite.RoundTripper.CaptureRoundTrip(req)
-					if err != nil {
-						tlog.Logf(t, "Request failed, not ready yet: %v (after %v)", err.Error(), elapsed)
-						return false
-					}
-
-					if err := http.CompareRoundTrip(t, &req, cReq, cRes, expectedResponse); err != nil {
-						tlog.Logf(t, "Response expectation failed for request: %+v  not ready yet: %v (after %v)", req, err, elapsed)
-						return false
-					}
-
-					return true
-				})
-				tlog.Logf(t, "Request passed")
+				assertConsistentRetryBehaviour(t, suite, gwAddr, ns, tc.args.path, tc.args.retrySimulationConfig, tc.want)
 			})
 		}
 	},
+}
+
+func assertConsistentRetryBehaviour(t *testing.T, suite *confsuite.ConformanceTestSuite, gwAddr string, ns string, path string, values url.Values, response http.Response) {
+	http.AwaitConvergence(t, suite.TimeoutConfig.RequiredConsecutiveSuccesses, suite.TimeoutConfig.MaxTimeToConsistency, func(elapsed time.Duration) bool {
+		// regenerate UUID on each probe so the retry simulation handler in the echo-basic backend tracks retries independently
+		values.Set("uuid", uuid.New().String())
+
+		expectedResponse := http.ExpectedResponse{
+			Request: http.Request{
+				Path: path + "?" + values.Encode(),
+			},
+			Response:  response,
+			Backend:   confsuite.InfraBackendServiceNameV3,
+			Namespace: ns,
+		}
+
+		req := http.MakeRequest(t, &expectedResponse, gwAddr, "HTTP", "http")
+
+		cReq, cRes, err := suite.RoundTripper.CaptureRoundTrip(req)
+		if err != nil {
+			tlog.Logf(t, "Request failed, not ready yet: %v (after %v)", err.Error(), elapsed)
+			return false
+		}
+
+		if err := http.CompareRoundTrip(t, &req, cReq, cRes, expectedResponse); err != nil {
+			tlog.Logf(t, "Response expectation failed for request: %+v  not ready yet: %v (after %v)", req, err, elapsed)
+			return false
+		}
+
+		return true
+	})
+	tlog.Logf(t, "Request passed")
 }
