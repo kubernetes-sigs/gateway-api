4b6e014b776796341e04b9074a4fe2f1b10f7086
Srujan rai <raisrujan@gmail.com>
2026-06-12 19:40:56 +0530

    conformance: add BackendRef URLRewrite and RequestRedirect tests (#4823)
    * conformance: add BackendRef URLRewrite and RequestRedirect tests (#2937, #2936)
    
    Add SupportHTTPRouteBackendURLRewrite and
    SupportHTTPRouteBackendRequestRedirect feature constants so implementations
    can declare Extended support for these filters at the HTTPBackendRef level.
    
    Add focused conformance tests covering ReplaceFullPath and
    ReplacePrefixMatch for each filter type applied via backendRef.filters,
    rather than at the rule level.
    
    * Update conformance/tests/httproute-backend-url-rewrite.go
    
    Co-authored-by: Norwin Schnyder <github@snorwin.io>
    
    * conformance: fix copyright header in httproute-backend-request-redirect
    
    ---------
    
    Co-authored-by: Norwin Schnyder <github@snorwin.io>

---
+++ conformance/tests/httproute-backend-request-redirect.go
@@ -0,0 +1,87 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tests
+
+import (
+	"testing"
+
+	"k8s.io/apimachinery/pkg/types"
+
+	"sigs.k8s.io/gateway-api/conformance/utils/http"
+	"sigs.k8s.io/gateway-api/conformance/utils/kubernetes"
+	"sigs.k8s.io/gateway-api/conformance/utils/roundtripper"
+	confsuite "sigs.k8s.io/gateway-api/conformance/utils/suite"
+	"sigs.k8s.io/gateway-api/pkg/features"
+)
+
+func init() {
+	ConformanceTests = append(ConformanceTests, HTTPRouteBackendRequestRedirect)
+}
+
+var HTTPRouteBackendRequestRedirect = confsuite.ConformanceTest{
+	ShortName:   "HTTPRouteBackendRequestRedirect",
+	Description: "An HTTPRoute with RequestRedirect filter applied at the BackendRef level",
+	Manifests:   []string{"tests/httproute-backend-request-redirect.yaml"},
+	Features: []features.FeatureName{
+		features.SupportGateway,
+		features.SupportHTTPRoute,
+		features.SupportHTTPRouteBackendRequestRedirect,
+	},
+	Test: func(t *testing.T, suite *confsuite.ConformanceTestSuite) {
+		ns := confsuite.InfrastructureNamespace
+		routeNN := types.NamespacedName{Name: "backend-request-redirect", Namespace: ns}
+		gwNN := types.NamespacedName{Name: "same-namespace", Namespace: ns}
+		gwAddr := kubernetes.GatewayAndHTTPRoutesMustBeAccepted(t, suite.Client, suite.TimeoutConfig, suite.ControllerName, kubernetes.NewGatewayRef(gwNN), routeNN)
+		kubernetes.HTTPRouteMustHaveResolvedRefsConditionsTrue(t, suite.Client, suite.TimeoutConfig, routeNN, gwNN)
+
+		testCases := []http.ExpectedResponse{
+			{
+				Request: http.Request{
+					Path:             "/full/original",
+					UnfollowRedirect: true,
+				},
+				Response: http.Response{
+					StatusCode: 302,
+				},
+				RedirectRequest: &roundtripper.RedirectRequest{
+					Path: "/redirected-full",
+				},
+				Namespace: ns,
+			},
+			{
+				Request: http.Request{
+					Path:             "/prefix/original",
+					UnfollowRedirect: true,
+				},
+				Response: http.Response{
+					StatusCode: 302,
+				},
+				RedirectRequest: &roundtripper.RedirectRequest{
+					Path: "/redirected-prefix/original",
+				},
+				Namespace: ns,
+			},
+		}
+		for i := range testCases {
+			tc := testCases[i]
+			t.Run(tc.GetTestCaseName(i), func(t *testing.T) {
+				t.Parallel()
+				http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gwAddr, tc)
+			})
+		}
+	},
+}
