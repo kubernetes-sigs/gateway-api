36c46d297a6ceb0479ade9423faabbefc60d9e24
Norwin Schnyder <norwin.schnyder+github@gmail.com>
2026-09-22 09:20:08 +0200

    Update GEP to restrict retriable status codes to 400–599
    Signed-off-by: Norwin Schnyder <norwin.schnyder+github@gmail.com>

--- apis/v1/httproute_types.go
+++ apis/v1/httproute_types.go
@@ -478,18 +478,11 @@ type HTTPRouteRetry struct {
 // Implementations MAY support specifying additional discrete values in the
 // 500-599 range.
 //
-// Implementations SHOULD NOT support retrying status codes in the 100-399
-// range, as these responses are generally not appropriate to retry.
-//
 // Implementations MAY support specifying discrete values in the 400-499 range,
 // which are often inadvisable to retry.
 //
-// Implementations MAY support discrete values in the 600-999 (inclusive)
-// range, which are not valid for HTTP clients, but are sometimes used for
-// communicating application-specific errors.
-//
-// +kubebuilder:validation:Minimum:=100
-// +kubebuilder:validation:Maximum:=999
+// +kubebuilder:validation:Minimum:=400
+// +kubebuilder:validation:Maximum:=599
 // <gateway:experimental>
 type HTTPRouteRetryStatusCode int
 
