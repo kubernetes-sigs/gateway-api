8d8295a06a0c3287e0c4bb27ed31c0cf9bea59b2
Yashraj Shukla <shuklayashraj68@gmail.com>
2026-09-23 08:46:14 +0000

    wasm: add missing func main() to non-wasm build stub
    Signed-off-by: Yashraj Shukla <shuklayashraj68@gmail.com>

--- wasm/stub.go
+++ wasm/stub.go
@@ -19,3 +19,5 @@ limitations under the License.
 // Package main is built only for GOOS=js GOARCH=wasm; see main.go.
 // This stub allows the package to load when not building for WebAssembly (e.g. go list ./..., linters).
 package main
+
+func main() {}
